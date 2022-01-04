8d67777facd70f38585cdaffa46d4873d95faba1
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-04 13:40:31 +0530

    TE5-T197 completed:practical-2

---
+++ react-day2/practical-02/src/components/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import Hello from "./Hello";
+import styles from "./App.module.css";
+import logo from "../assets/logo.svg";
+
+export const App = () => {
+	const [counter, setCounter] = useState(0);
+
+	return (
+		<>
+			<h1
+				className={styles.h1}
+				onClick={() => {
+					setCounter((prevCount) => prevCount + 1);
+				}}
+			>
+				count : {counter}
+			</h1>
+			<p>sample svg</p>
+			<img src={logo} alt="logo" />
+			<p>Hello component</p>
+			<Hello />
+		</>
+	);
+};
