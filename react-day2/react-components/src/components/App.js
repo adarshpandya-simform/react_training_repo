e04737ed815474df732965e7b1b0082d53d0f716
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-04 11:39:35 +0530

    TE5-T197 added class component

--- react-day2/react-components/src/components/App.js
+++ react-day2/react-components/src/components/App.js
@@ -1,21 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
+import ClassComponent from "./ClassComponent/ClassComponent";
 // import styles from "./App.module.css";
 
 export const App = () => {
-	const [counter, setCounter] = useState(0);
-
 	return (
 		<>
-			<h1
-				// className={styles.h1}
-				onClick={() => {
-					setCounter((prevCount) => prevCount + 1);
-				}}
-			>
-				count : {counter}
-			</h1>
-			<p>sample svg</p>
-			{/* <img src={logo} alt="logo" /> */}
+			<h4>four ways of creating components</h4>
+			<p>Funcional Component</p>
+			<p>Class Component</p>
+			<ClassComponent />
+			<p>HOC</p>
+			<p>Pure Component</p>
 		</>
 	);
 };
--- react-day2/react-components/src/index.js
+++ react-day2/react-components/src/index.js
@@ -1,9 +1,7 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
-// import { App } from "./components/App";
+import { App } from "./components/App";
 import "./index.css";
-
-const App = () => <h1>hi</h1>;
 
 ReactDOM.render(
 	<StrictMode>
