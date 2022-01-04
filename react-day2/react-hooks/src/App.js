3586736495431275a55b18da2a69810f07ef211a
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-04 14:19:38 +0530

    TE5-T197 added:useContext with provider

--- react-day2/react-hooks/src/App.js
+++ react-day2/react-hooks/src/App.js
@@ -1,9 +1,13 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import "./App.css";
 import Counter from "./components/Counter/Counter";
+import { DarkThemeContext } from "./context/DarkMode";
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
+
+  // useContext hook for theme
+  const { toggleTheme, theme } = useContext(DarkThemeContext);
 
   // useEffect hook example (run when component mounts)
   useEffect(() => {
@@ -14,7 +18,18 @@ function App() {
 
   return (
     <div>
+      {/* useState and useEffect Example */}
       {isLoading ? <p>fetching counter give me some time</p> : <Counter />}
+      {/* useContext Example */}
+      <p>change theme with context</p>
+      <button
+        className={theme === "light" ? "light-btn" : "dark-btn"}
+        onClick={toggleTheme}
+      >
+        {theme === "light"
+          ? "click to apply dark theme"
+          : "click to apply light theme"}
+      </button>
     </div>
   );
 }
