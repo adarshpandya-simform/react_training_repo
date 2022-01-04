6aac9f4da75a96ed7c5f5f85704f22f9ed5bc042
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-04 12:27:02 +0530

    TE5-T197 refactored: HOC

--- react-day2/react-components/src/components/HigherOrderComponent/WithList.js
+++ react-day2/react-components/src/components/HigherOrderComponent/WithList.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-export default function WithList (WrappedComponent) {
-	return function withLoading({ isLoading }) {
-		if (isLoading)
-		{
+const WithList = (WrappedComponent) => {
+	const withLoading = ({ isLoading }) => {
+		if (isLoading) {
 			return <p>loading...</p>;
 		} else {
 			return <WrappedComponent />;
 		}
 	};
-}
+	return withLoading;
+};
+export default WithList;
