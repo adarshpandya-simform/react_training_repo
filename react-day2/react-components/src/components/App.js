b900e7456e645c2289dbddb6bb6a1462f77f6c27
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-04 12:22:57 +0530

    added:functional component

--- react-day2/react-components/src/components/App.js
+++ react-day2/react-components/src/components/App.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ClassComponent from "./ClassComponent/ClassComponent";
 import FunctionalComponent from "./FunctionalComponent/FunctionalComponent";
 import Purecomponent from "./PureComponent/PureComponent";
-// import styles from "./App.module.css";
+import WithList from "./HigherOrderComponent/WithList";
+import List from "./HigherOrderComponent/List";
+
+const WithListLoad = WithList(List);
 
 export const App = () => {
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setIsLoading(false);
+		}, 3000);
+	}, []);
+
 	return (
 		<>
 			<h4>four ways of creating components</h4>
@@ -13,6 +24,7 @@ export const App = () => {
 			<p>Class Component</p>
 			<ClassComponent />
 			<p>HOC</p>
+			<WithListLoad isLoading={isLoading} />
 			<p>Pure Component</p>
 			<Purecomponent />
 		</>
