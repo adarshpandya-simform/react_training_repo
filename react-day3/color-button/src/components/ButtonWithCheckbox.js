975f403c9b3d03af77367fc8cef7c26034383078
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-05 10:20:55 +0530

    TE5-T197 added: buttonWithCheckbox test

--- react-day3/color-button/src/components/ButtonWithCheckbox.js
+++ react-day3/color-button/src/components/ButtonWithCheckbox.js
@@ -2,18 +2,24 @@ import { useState } from "react";
 
 const ButtonWithCheckbox = () => {
   const [color, setColor] = useState("blue");
-
+  const [isDisabled, setIsDisabled] = useState(false);
   return (
     <div>
       <button
         style={{ background: color }}
+        disabled={isDisabled}
         onClick={() => {
           setColor((prevColor) => (prevColor === "blue" ? "red" : "blue"));
         }}
       >
         {color === "blue" ? "change to red" : "change to blue"}
       </button>
-      <input type="checkbox" />
+      <input
+        type="checkbox"
+        onClick={() => {
+          setIsDisabled((prev) => !prev);
+        }}
+      />
     </div>
   );
 };
