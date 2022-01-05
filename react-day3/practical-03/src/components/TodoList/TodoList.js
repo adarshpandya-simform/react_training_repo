31d1364cf9570cbe048fb1b1b4b025394ca035e7
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-05 14:58:10 +0530

    TE5-T197 added comments for TodoList Component

--- react-day3/practical-03/src/components/TodoList/TodoList.js
+++ react-day3/practical-03/src/components/TodoList/TodoList.js
@@ -2,14 +2,17 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./TodoList.css";
 
+// TodoList component which accepts 'todos' as prop
 const TodoList = ({ todos }) => {
 	return (
 		<div className="todo-app-list">
+			{/* render a msg if no todos */}
 			{todos.length === 0 && (
 				<p className="empty-msg">
 					yay!! all tasks are completed! start adding new tasks
 				</p>
 			)}
+			{/* map through todos and display them */}
 			{todos.map((todo) => (
 				<div key={todo} className="todo-display">
 					<p>{todo}</p>
@@ -19,6 +22,7 @@ const TodoList = ({ todos }) => {
 	);
 };
 
+// defining propTypes for TodoList
 TodoList.propTypes = { todos: PropTypes.array };
 
 export default TodoList;
