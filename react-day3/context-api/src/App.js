2539c76709c4b1e2115f06a338249fdb55d7e701
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-05 17:03:57 +0530

    TE5-T197 added complete/incomplete todos component

--- react-day3/context-api/src/App.js
+++ react-day3/context-api/src/App.js
@@ -1,13 +1,25 @@
 import InputTodo from "./components/InputTodo/InputTodo";
 import DisplayTodo from "./components/DisplayTodo/DisplayTodo";
+import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <p>a todo app with global context</p>
-      <InputTodo />
-      <DisplayTodo />
+      <p>a todo app with global context</p> <br />
+      <br />
+      <Link to={"/"}>home</Link> <br />
+      <Link to={"/add-todo"}>add todo</Link> <br />
+      <Link to={"/show-all-todo"}>show all todos</Link> <br />
+      <Link to={"/completed-todo"}>show only completed todos</Link> <br />
+      <Link to={"/incompleted-todo"}>show only incompleted todos</Link>
+      <Routes>
+        <Route path="/" element={<p>you are at home page</p>} />
+        <Route path="/add-todo" element={<InputTodo />} />
+        <Route path="/show-all-todo" element={<DisplayTodo />} />
+        {/* <Route path="/" element={<p>you are at home page</p>} /> */}
+        {/* <Route path="/" element={<p>you are at home page</p>} /> */}
+      </Routes>
     </div>
   );
 }
---
+++ react-day3/context-api/src/components/IncompleteTodos/IncompleteTodos.js
@@ -0,0 +1,49 @@
+import React, { useContext } from "react";
+import { CHECK_TODO, DELETE_TODO } from "../../context/todo.action";
+import { TodosContext } from "../../context/todo.context";
+
+const IncompleteTodos = () => {
+  const { state, dispatch } = useContext(TodosContext);
+
+  const checkHandler = (e, id) => {
+    dispatch({
+      type: CHECK_TODO,
+      payload: { id, completed: e.target.checked },
+    });
+  };
+
+  const deleteHandler = (id) => {
+    dispatch({ type: DELETE_TODO, payload: { id } });
+  };
+
+  return (
+    <div>
+      <p>incomplete todos</p>
+      {state.todos.map((todo) => (
+        <div key={todo.id}>
+          <input
+            type="checkbox"
+            onChange={(e) => {
+              checkHandler(e, todo.id);
+            }}
+            checked={todo.completed}
+          />
+          <span
+            style={todo.completed ? { textDecoration: "line-through" } : {}}
+          >
+            {todo.title}
+          </span>
+          <button
+            onClick={() => {
+              deleteHandler(todo.id);
+            }}
+          >
+            x
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default IncompleteTodos;
