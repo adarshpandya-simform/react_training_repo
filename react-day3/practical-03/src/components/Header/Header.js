b1848ea6a6c425a23876d3a8c8e779c35afea40a
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-05 11:56:45 +0530

    TE5-T197 completed: styling the header component

--- react-day3/practical-03/src/components/Header/Header.js
+++ react-day3/practical-03/src/components/Header/Header.js
@@ -2,7 +2,15 @@ import React from "react";
 import "./Header.css";
 
 const Header = () => {
-	return <div className="todo-app-header">header</div>;
+	return (
+		<div className="todo-app-header">
+			<p>My Todos(5)</p>
+			<div className="date-and-time">
+				<span>Jan 5,2022</span>
+				<span>11:48 AM</span>
+			</div>
+		</div>
+	);
 };
 
 export default Header;
