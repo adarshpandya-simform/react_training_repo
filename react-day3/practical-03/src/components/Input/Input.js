91a5ffcaa4c983ff045f7674e564807d455f8613
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-05 12:33:22 +0530

    TE5-T197 completed Input component styling and functionality

--- react-day3/practical-03/src/components/Input/Input.js
+++ react-day3/practical-03/src/components/Input/Input.js
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Input.css";
 
 const Input = () => {
-	return <div className="todo-app-input">input component</div>;
+	const [toggleInput, setToggleInput] = useState(false);
+	const [todo, setTodo] = useState("");
+
+	const toggleHandler = () => {
+		setToggleInput((prevToggle) => !prevToggle);
+	};
+
+	const handleKey = (event) => {
+		if (event.key === "Enter") {
+			//logic to add todo in localStorage
+			setTodo("");
+		}
+		if (event.key === "Escape") {
+			toggleHandler();
+		}
+	};
+
+	return (
+		<div className="todo-app-input">
+			{toggleInput && (
+				<input
+					type="text"
+					placeholder="Enter Todo"
+					className="todo-app-main-input"
+					value={todo}
+					onChange={(e) => {
+						setTodo(e.target.value);
+					}}
+					autoFocus
+					onKeyUp={handleKey}
+				/>
+			)}
+			{!toggleInput && (
+				<button onClick={toggleHandler} className="todo-app-add-button">
+					+
+				</button>
+			)}
+		</div>
+	);
 };
 
 export default Input;
