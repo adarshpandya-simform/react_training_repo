9a391810a5c5e4f0c5c1d78e089421e72232ca7f
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-05 11:47:08 +0530

    TE5-T197 added:basic styling

--- react-day3/practical-03/src/components/App.js
+++ react-day3/practical-03/src/components/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header/Header";
+import TodoList from "./TodoList/TodoList";
+import Input from "./Input/Input";
 
 const App = () => {
 	return (
 		<div className="app-container">
 			<div className="app-title">Todo App</div>
-			<div className="todo-parent"></div>
+			<div className="todo-parent">
+				<Header />
+				<TodoList />
+				<Input />
+			</div>
 		</div>
 	);
 };
--- react-day3/practical-03/src/components/TodoList/TodoList.js
+++ react-day3/practical-03/src/components/TodoList/TodoList.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./TodoList.css";
 
 const TodoList = () => {
-	return <div>listhere</div>;
+	return <div className="todo-app-list"></div>;
 };
 
 export default TodoList;
