e85d966af25140e784b6bcd6b5c512b278def3e6
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-05 12:12:48 +0530

    TE5-T197 added a helper function for date

---
+++ react-day3/practical-03/src/helpers/getDate.js
@@ -0,0 +1,23 @@
+export const getDate = (() => {
+	const monthNames = [
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December",
+	];
+	return () => {
+		const dateObj = new Date();
+		const month = monthNames[dateObj.getMonth()];
+		const day = String(dateObj.getDate()).padStart(2, "0");
+		const year = dateObj.getFullYear();
+		return `${month} ${day}, ${year}`;
+	};
+})();
