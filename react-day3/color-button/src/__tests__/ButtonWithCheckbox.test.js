0ae675bbb6bb2bc531ae7a752038b4b825bf787b
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-05 10:23:00 +0530

    TE5-T197 refactored: added different colors in buttonWithCheckbox component

--- react-day3/color-button/src/__tests__/ButtonWithCheckbox.test.js
+++ react-day3/color-button/src/__tests__/ButtonWithCheckbox.test.js
@@ -3,16 +3,16 @@ import ButtonWithCheckbox from "../components/ButtonWithCheckbox";
 
 test("renders button with checkbox", () => {
   render(<ButtonWithCheckbox />);
-  const colorButton = screen.getByRole("button", { name: "change to red" });
+  const colorButton = screen.getByRole("button", { name: "change to green" });
   const checkbox = screen.getByRole("checkbox");
-  expect(colorButton).toHaveStyle({ background: "blue" });
+  expect(colorButton).toHaveStyle({ background: "yellow" });
   expect(colorButton).toBeEnabled();
   expect(checkbox).not.toBeChecked();
 });
 
 test("should toggle the button state enable/disabled when clicking on checkbox", () => {
   render(<ButtonWithCheckbox />);
-  const colorButton = screen.getByRole("button", { name: "change to red" });
+  const colorButton = screen.getByRole("button", { name: "change to green" });
   const checkbox = screen.getByRole("checkbox");
   fireEvent.click(checkbox);
   expect(colorButton).not.toBeEnabled();
