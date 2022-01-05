992e2239a7a86a46d0e2ffafb47f47d170f6243e
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-05 13:39:13 +0530

    TE5-T197 completed functionality in App component

--- react-day3/practical-03/src/components/App.js
+++ react-day3/practical-03/src/components/App.js
@@ -1,28 +1,29 @@
 import React, { useEffect, useState } from "react";
-import "./App.css";
 import Header from "./Header/Header";
 import TodoList from "./TodoList/TodoList";
 import Input from "./Input/Input";
 import { getDate } from "../helpers/getDate";
+import "./App.css";
 
 const App = () => {
 	const [todos, setTodos] = useState([]);
 
 	// useEffect to fetch todos on Load
 	useEffect(() => {
-		// let todayDate = localStorage.getItem("date");
-		// let todosFromFetch = localStorage.getItem("todos");
-		// if (todayDate === getDate()) {
-		// 	console.log(JSON.parse(todosFromFetch));
-		// } else {
-		// 	localStorage.setItem("date", JSON.stringify(getDate()));
-		// }
-		// console.log(todayDate);
+		let todayDate = localStorage.getItem("date");
+		let todosFromFetch = localStorage.getItem("todos");
+		if (JSON.parse(todayDate) === getDate()) {
+			setTodos(JSON.parse(todosFromFetch));
+		} else {
+			setTodos([]);
+			localStorage.setItem("todos", JSON.stringify(todos));
+			localStorage.setItem("date", JSON.stringify(getDate()));
+		}
 	}, []);
 
 	// useEffect to set todos in localStorage when they change
 	useEffect(() => {
-		// localStorage.setItem("todos", JSON.stringify(todos));
+		localStorage.setItem("todos", JSON.stringify(todos));
 	}, [todos]);
 
 	// add todo function
