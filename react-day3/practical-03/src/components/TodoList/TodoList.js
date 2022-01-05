00ba87e9c3b77a28ec2e91e1376f10db5adf6a26
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-05 12:59:38 +0530

    TE5-T197 completed: TodoList component styling and functionality

--- react-day3/practical-03/src/components/TodoList/TodoList.js
+++ react-day3/practical-03/src/components/TodoList/TodoList.js
@@ -1,8 +1,24 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./TodoList.css";
 
-const TodoList = () => {
-	return <div className="todo-app-list"></div>;
+const TodoList = ({ todos }) => {
+	return (
+		<div className="todo-app-list">
+			{todos.length === 0 && (
+				<p className="empty-msg">
+					yay!! all tasks are completed! start adding new tasks
+				</p>
+			)}
+			{todos.map((todo) => (
+				<div key={todo} className="todo-display">
+					<p>{todo}</p>
+				</div>
+			))}
+		</div>
+	);
 };
+
+TodoList.propTypes = { todos: PropTypes.array };
 
 export default TodoList;
