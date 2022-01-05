a1fa6833a8e27d4e5ff62d780d9600cc7f56a496
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-05 12:19:16 +0530

    TE5-T197 completed

--- react-day3/practical-03/src/components/Header/Header.js
+++ react-day3/practical-03/src/components/Header/Header.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
+import { getDate } from "../../helpers/getDate";
 import "./Header.css";
 
 const Header = () => {
+	const date = useMemo(() => getDate(), [date]);
+	const [time, setTime] = useState(new Date());
+
+	useEffect(() => {
+		let TimeId = setInterval(() => setTime(new Date()), 1000);
+		return () => {
+			clearInterval(TimeId);
+		};
+	});
+
 	return (
 		<div className="todo-app-header">
-			<p>My Todos(5)</p>
+			<p>My Todos</p>
 			<div className="date-and-time">
-				<span>Jan 5,2022</span>
-				<span>11:48 AM</span>
+				<span>{date}</span>
+				<span>
+					{time.toLocaleTimeString().slice(0, 5)}{" "}
+					{time.toLocaleTimeString().slice(-2)}
+				</span>
 			</div>
 		</div>
 	);
