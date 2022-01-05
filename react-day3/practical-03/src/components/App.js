d39e32c866704caaff75fe1a1220ff317e57b4de
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-05 12:59:05 +0530

    TE5-T197 added: addTodo function and passed data as props to other components

--- react-day3/practical-03/src/components/App.js
+++ react-day3/practical-03/src/components/App.js
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./Header/Header";
 import TodoList from "./TodoList/TodoList";
 import Input from "./Input/Input";
+import { getDate } from "../helpers/getDate";
 
 const App = () => {
+	const [todos, setTodos] = useState([]);
+
+	// useEffect to fetch todos on Load
+	useEffect(() => {
+		// let todayDate = localStorage.getItem("date");
+		// let todosFromFetch = localStorage.getItem("todos");
+		// if (todayDate === getDate()) {
+		// 	console.log(JSON.parse(todosFromFetch));
+		// } else {
+		// 	localStorage.setItem("date", JSON.stringify(getDate()));
+		// }
+		// console.log(todayDate);
+	}, []);
+
+	// useEffect to set todos in localStorage when they change
+	useEffect(() => {
+		// localStorage.setItem("todos", JSON.stringify(todos));
+	}, [todos]);
+
+	// add todo function
+
+	const addTodo = (todo) => {
+		setTodos((prevTodos) => [...prevTodos, todo]);
+	};
+
 	return (
 		<div className="app-container">
 			<div className="app-title">Todo App</div>
 			<div className="todo-parent">
 				<Header />
-				<TodoList />
-				<Input />
+				<TodoList todos={todos} />
+				<Input addTodo={addTodo} />
 			</div>
 		</div>
 	);
