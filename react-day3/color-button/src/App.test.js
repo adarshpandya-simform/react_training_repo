d8fe18e0e7deb134492af1d5e54e35d66d6ff962
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-05 10:45:20 +0530

    TE5-T197 added:removeCamelCase function with tests in describe

--- react-day3/color-button/src/App.test.js
+++ react-day3/color-button/src/App.test.js
@@ -1,22 +1,36 @@
 import { fireEvent, render, screen } from "@testing-library/react";
-import App from "./App";
+import App, { removeCamelCase } from "./App";
 
-test("renders learn react link", () => {
+test("should render button with style", () => {
   render(<App />);
   const buttonElement = screen.getByRole("button", { name: "change to red" });
   expect(buttonElement).toHaveStyle({ background: "blue" });
-});
-
-test("on click color change", () => {
-  render(<App />);
-  const buttonElement = screen.getByRole("button", { name: "change to red" });
-  fireEvent.click(buttonElement);
-  expect(buttonElement).toHaveStyle({ background: "red" });
 });
 
 test("should change color on click", () => {
   render(<App />);
   const buttonElement = screen.getByRole("button", { name: "change to red" });
   fireEvent.click(buttonElement);
+  expect(buttonElement).toHaveStyle({ background: "red" });
+});
+
+test("should change text on click", () => {
+  render(<App />);
+  const buttonElement = screen.getByRole("button", { name: "change to red" });
+  fireEvent.click(buttonElement);
   expect(buttonElement.textContent).toBe("change to blue");
+});
+
+describe("spaces before camelcase letters", () => {
+  test("should work for no inner capital letter", () => {
+    expect(removeCamelCase("Red")).toBe("Red");
+  });
+
+  test("should work for one inner capital letter", () => {
+    expect(removeCamelCase("MidnightBlue")).toBe("Midnight Blue");
+  });
+
+  test("should work for multiple inner capital letters", () => {
+    expect(removeCamelCase("MediumVioletRed")).toBe("Medium Violet Red");
+  });
 });
