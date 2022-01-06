5400fcc11c45135047b35f57952419553754ed85
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-06 17:17:59 +0530

    TE5-T197 completed Options and ScoopOptions component

--- react-day4/sundaes-app-testing-course/src/components/Options.js
+++ react-day4/sundaes-app-testing-course/src/components/Options.js
@@ -1,5 +1,30 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import ScoopOptions from "./ScoopOptions";
+import Row from "react-bootstrap/Row";
+
 const Options = ({ optionType }) => {
-  return <div>options</div>;
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    (async () => {
+      await axios
+        .get(`http://localhost:3030/${optionType}`)
+        .then((res) => {
+          setItems(res.data);
+        })
+        .catch((err) => console.log(err));
+    })();
+  }, [optionType]);
+
+  const ItemComponent = optionType === "scoops" ? ScoopOptions : null;
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
+  return <Row>{optionItems}</Row>;
 };
 
 export default Options;
