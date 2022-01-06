c666358d33ae120887438c18e737ac92ef97dcc2
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-06 11:32:12 +0530

    added <Link> for navigation in home page

--- react-day4/e-com/src/components/HomePage/HomePage.js
+++ react-day4/e-com/src/components/HomePage/HomePage.js
@@ -5,6 +5,9 @@ const HomePage = () => {
   return (
     <div>
       <h4>E-Commerce home</h4>
+      <Link to={"/products"}>go to view products</Link>
+      <br />
+      <Link to={"/login"}>login here</Link>
     </div>
   );
 };
