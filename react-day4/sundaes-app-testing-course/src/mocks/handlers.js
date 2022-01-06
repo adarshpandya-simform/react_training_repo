bd47d544078e681850aa10e9a42c241d5c24cc92
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-06 16:37:45 +0530

    TE5-T197 added: mock handler

--- react-day4/sundaes-app-testing-course/src/mocks/handlers.js
+++ react-day4/sundaes-app-testing-course/src/mocks/handlers.js
@@ -1,1 +1,12 @@
 import { rest } from "msw";
+
+export const handlers = [
+  rest.get("http://localhost:3030/scoops", (req, res, context) => {
+    return res(
+      context.json([
+        { name: "Chocolate", imagePath: "/images/chocolate.png" },
+        { name: "Vanilla", imagePath: "/images/anilla.png" },
+      ])
+    );
+  }),
+];
