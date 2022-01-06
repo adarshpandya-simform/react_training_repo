38ce22314dac493235a0c59d11d2cb66e2e6abb0
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-06 14:11:46 +0530

    TE5-T197 added:Link to home in Login component

--- react-day4/e-com/src/components/Login/Login.js
+++ react-day4/e-com/src/components/Login/Login.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [username, setUserName] = useState("");
@@ -43,6 +43,9 @@ const Login = () => {
       />{" "}
       <br />
       <button onClick={handleSubmit}>login</button>
+      <br />
+      <br />
+      <Link to={"/"}>go back to home</Link>
     </div>
   );
 };
