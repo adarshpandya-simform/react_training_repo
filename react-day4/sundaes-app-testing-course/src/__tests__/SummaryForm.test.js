732820c92eed53b008e980036b345d7f1589df10
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-06 15:46:29 +0530

    TE5-T197 added: Popover hover/unhover tests

--- react-day4/sundaes-app-testing-course/src/__tests__/SummaryForm.test.js
+++ react-day4/sundaes-app-testing-course/src/__tests__/SummaryForm.test.js
@@ -1,4 +1,8 @@
-import { screen, render } from "@testing-library/react";
+import {
+  screen,
+  render,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import SummaryForm from "../components/SummaryForm";
 import userEvent from "@testing-library/user-event";
 
@@ -35,4 +39,25 @@ test("should enable button when checking checkbox", () => {
   userEvent.click(checkboxElem);
   expect(buttonElem).toBeDisabled();
   expect(checkboxElem).not.toBeChecked();
+});
+
+test("popover should respond on hover", async () => {
+  render(<SummaryForm />);
+  // popover starts hidden
+  const popover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(popover).not.toBeInTheDocument();
+  // appears
+  const tAndC = screen.getByText(/terms and conditions/i);
+  userEvent.hover(tAndC);
+
+  let popOver2 = screen.getByText("no ice cream will actually be delivered");
+  expect(popOver2).toBeInTheDocument();
+  // hidden when mouse out
+  userEvent.unhover(tAndC);
+
+  await waitForElementToBeRemoved(() =>
+    screen.queryByText("no ice cream will actually be delivered")
+  );
 });
