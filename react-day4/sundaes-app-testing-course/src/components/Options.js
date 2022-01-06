057076da5f7af571e783869fab6133dbbf00cb01
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-06 16:54:57 +0530

    TE5-T197 added:Scoopoptions and Options component

---
+++ react-day4/sundaes-app-testing-course/src/components/Options.js
@@ -0,0 +1,5 @@
+const Options = ({ optionType }) => {
+  return <div>options</div>;
+};
+
+export default Options;
---
+++ react-day4/sundaes-app-testing-course/src/components/ScoopOptions.js
@@ -0,0 +1,5 @@
+const ScoopOptions = () => {
+  return <div>scoop options</div>;
+};
+
+export default ScoopOptions;
