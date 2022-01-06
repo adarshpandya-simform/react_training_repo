7165c02cded4a1a31f88a174b4d8be071e3cce49
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-06 17:29:02 +0530

    TE5-T197 tried rendering Options in App component

--- react-day4/sundaes-app-testing-course/src/App.js
+++ react-day4/sundaes-app-testing-course/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Options from "./components/Options";
 import SummaryForm from "./components/SummaryForm";
 
 function App() {
   return (
     <div>
       <SummaryForm />
+      <Options optionType={"scoops"} />
     </div>
   );
 }
