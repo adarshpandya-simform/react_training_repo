243ff6cd855c863f63e63ccf526e274eb3f4cde6
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-07 12:42:42 +0530

    TE5-T97 completed the styling and functionality of TodoList

--- react-day4/practical-04/src/components/TodoList/TodoList.js
+++ react-day4/practical-04/src/components/TodoList/TodoList.js
@@ -1,8 +1,55 @@
-import React from "react";
+import React, { useContext } from "react";
+import { CHECK_TODO, DELETE_TODO } from "../../context/todo.actions";
+import { TodoContext } from "../../context/todo.context";
 import "./TodoList.css";
 
 const TodoList = () => {
-	return <div className="todo-list-parent">todo list component</div>;
+	const { state, dispatch } = useContext(TodoContext);
+
+	const handleCheckboxToggle = (e, id) => {
+		dispatch({
+			type: CHECK_TODO,
+			payload: { id, completed: e.target.checked },
+		});
+	};
+
+	const handleDeleteTodo = (id) => {
+		dispatch({ type: DELETE_TODO, payload: { id } });
+	};
+
+	return (
+		<div className="todo-list-parent">
+			{state.todos.length === 0 && (
+				<span className="empty-msg">
+					Yayy! No pending Todos Today. Start adding them with + button
+				</span>
+			)}
+			{state.todos.map(({ id, title, completed }) => (
+				<div
+					onDoubleClick={() => {
+						handleDeleteTodo(id);
+					}}
+					key={id}
+					className="todo-msg-parent"
+				>
+					<span className={completed ? "todo-msg completed" : "todo-msg"}>
+						{title}
+					</span>
+
+					<label className="control control-checkbox">
+						<input
+							type="checkbox"
+							checked={completed}
+							onChange={(e) => {
+								handleCheckboxToggle(e, id);
+							}}
+						/>
+						<div className="control_indicator"></div>
+					</label>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default TodoList;
