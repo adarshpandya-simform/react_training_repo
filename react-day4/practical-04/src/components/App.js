ebbd7bed93e2e9909bfe0379117d1e9129d63220
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-07 09:50:46 +0530

    TE5-T197 created: App component

---
+++ react-day4/practical-04/src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+	return (
+		<div className="todo-app-container">
+			<p>hello world</p>
+		</div>
+	);
+};
+
+export default App;
