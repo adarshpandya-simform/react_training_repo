1ce5ae6476c59c846fed71aa930692557f8819cb
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-07 10:57:29 +0530

    TE5-T197 completed Header component

--- react-day4/practical-04/src/components/Header/Header.js
+++ react-day4/practical-04/src/components/Header/Header.js
@@ -1,8 +1,25 @@
 import React from "react";
+import { getDate } from "../../helpers/getDate";
+import { getDayName } from "../../helpers/getDay";
 import "./Header.css";
 
 const Header = () => {
-	return <div className="header-parent">Header component</div>;
+	const { day: date, month, year } = getDate();
+	const day = getDayName();
+	return (
+		<div className="header-parent">
+			<div className="header-details">
+				<div className="header-date">
+					<span className="current-date">{date}</span>
+				</div>
+				<div className="header-month-year">
+					<span className="current-month">{month}</span>
+					<span className="current-year">{year}</span>
+				</div>
+			</div>
+			<div className="header-day">{day.toUpperCase()}</div>
+		</div>
+	);
 };
 
 export default Header;
