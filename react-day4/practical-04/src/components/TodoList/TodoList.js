d5ea7b0cf7cdf0cee15711ced7b134055c070600
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-07 13:57:53 +0530

    TE5-T197 added: comments in TodoList component

--- react-day4/practical-04/src/components/TodoList/TodoList.js
+++ react-day4/practical-04/src/components/TodoList/TodoList.js
@@ -3,9 +3,12 @@ import { CHECK_TODO, DELETE_TODO } from "../../context/todo.actions";
 import { TodoContext } from "../../context/todo.context";
 import "./TodoList.css";
 
+// TodoList component
 const TodoList = () => {
+	// accessing state and dispatcher from global state context
 	const { state, dispatch } = useContext(TodoContext);
 
+	// handling checkbox toggle
 	const handleCheckboxToggle = (e, id) => {
 		dispatch({
 			type: CHECK_TODO,
@@ -13,17 +16,23 @@ const TodoList = () => {
 		});
 	};
 
+	// handling delete todo
 	const handleDeleteTodo = (id) => {
 		dispatch({ type: DELETE_TODO, payload: { id } });
 	};
 
 	return (
 		<div className="todo-list-parent">
+			{/* conditional rendering for todos */}
+
+			{/* checking if there are no todos */}
 			{state.todos.length === 0 && (
 				<span className="empty-msg">
 					Yayy! No pending Todos Today. Start adding them with + button
 				</span>
 			)}
+
+			{/* mapping through todos */}
 			{state.todos.map(({ id, title, completed }) => (
 				<div
 					onDoubleClick={() => {
@@ -36,6 +45,7 @@ const TodoList = () => {
 						{title}
 					</span>
 
+					{/* custom checkbox */}
 					<label className="control control-checkbox">
 						<input
 							type="checkbox"
