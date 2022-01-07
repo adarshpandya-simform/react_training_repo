d46b38e36e1cf0d460acbbd5bfc59675dff31d52
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-07 16:24:31 +0530

    TE5-T197 wrapper provider aournd components

--- react-day4/sundaes-app-testing-course/src/App.js
+++ react-day4/sundaes-app-testing-course/src/App.js
@@ -2,14 +2,17 @@ import "./App.css";
 import Options from "./components/Options";
 import OrderEntry from "./components/OrderEntry";
 import SummaryForm from "./components/SummaryForm";
+import { OrderDetailsProvider } from "./contexts/order.context";
 
 function App() {
   return (
     <div>
-      <SummaryForm />
-      <Options optionType={"scoops"} />
-      <Options optionType={"toppings"} />
-      <OrderEntry />
+      <OrderDetailsProvider>
+        <SummaryForm />
+        <Options optionType={"scoops"} />
+        <Options optionType={"toppings"} />
+        <OrderEntry />
+      </OrderDetailsProvider>
     </div>
   );
 }
