cbcaf82e1cae22145eab663a42534f5ee0eabe64
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-07 09:36:37 +0530

    TE5-T197 added: index files

---
+++ react-day4/practical-04/src/index.js
@@ -0,0 +1,13 @@
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+const App = () => <h1>hela</h1>;
+const root = document.getElementById("root");
+
+ReactDOM.render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+	root
+);
