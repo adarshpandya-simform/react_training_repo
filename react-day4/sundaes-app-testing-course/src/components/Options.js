26846f6248db19002f87e4982ef1857b4872837f
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-07 17:55:52 +0530

    TE5-T197 added: useOrderDetails() in Options

--- react-day4/sundaes-app-testing-course/src/components/Options.js
+++ react-day4/sundaes-app-testing-course/src/components/Options.js
@@ -4,10 +4,12 @@ import ScoopOptions from "./ScoopOptions";
 import Row from "react-bootstrap/Row";
 import ToppingOptions from "./ToppingOptions";
 import AlertBanner from "./AlertBanner";
+import { pricePerItem, useOrderDetails } from "../contexts/order.context";
 
 const Options = ({ optionType }) => {
   const [items, setItems] = useState([]);
   const [error, setError] = useState(false);
+  const [orderDetails, updateItemCount] = useOrderDetails();
 
   useEffect(() => {
     (async () => {
@@ -27,14 +29,28 @@ const Options = ({ optionType }) => {
   }
 
   const ItemComponent = optionType === "scoops" ? ScoopOptions : ToppingOptions;
+  const title = optionType[0].toUpperCase() + optionType.slice(1).toLowerCase();
+
   const optionItems = items.map((item) => (
     <ItemComponent
       key={item.name}
       name={item.name}
       imagePath={item.imagePath}
+      updateItemCount={(itemName, newItemCount) => {
+        updateItemCount(itemName, newItemCount, optionType);
+      }}
     />
   ));
-  return <Row>{optionItems}</Row>;
+  return (
+    <>
+      <h2>{title}</h2>
+      <p>{pricePerItem[optionType]} each</p>
+      <p>
+        {title} total: {orderDetails.totals[optionType]}
+      </p>
+      <Row>{optionItems}</Row>
+    </>
+  );
 };
 
 export default Options;
