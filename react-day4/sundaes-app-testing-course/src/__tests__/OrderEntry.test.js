bd97ae20e0771d73a3122519941d7e7903d9d0b8
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-07 15:46:51 +0530

    TE5-T197 tested: OrderEntry component with OrderEntry.test.js

---
+++ react-day4/sundaes-app-testing-course/src/__tests__/OrderEntry.test.js
@@ -0,0 +1,30 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import OrderEntry from "../components/OrderEntry";
+import { rest } from "msw";
+import { server } from "../mocks/server";
+
+test.only("should handle error for scoop and toppings route", async () => {
+  // reset handlers
+  server.resetHandlers(
+    rest.get("http://localhost:3030/scoops", (req, res, context) => {
+      return res(context.status(500));
+    }),
+    rest.get("http://localhost:3030/toppings", (req, res, context) => {
+      return res(context.status(500));
+    })
+  );
+
+  // render component
+  render(<OrderEntry />);
+
+  await waitFor(async () => {
+    const alert = await screen.findAllByRole("alert");
+
+    expect(alert).toHaveLength(2);
+  });
+});
+
+// both of this tests will be skipper
+test("should skip this test", () => {});
+
+test.skip("this will also be skipped", () => {});
