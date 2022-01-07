35cf86df6e37a8c37568d67c30bf7bc3df821889
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-07 10:14:06 +0530

    TE5-T197 added: TodoProvider around App

--- react-day4/practical-04/src/index.js
+++ react-day4/practical-04/src/index.js
@@ -1,13 +1,16 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { TodoProvider } from "./context/todo.context";
 import "./index.css";
 
 const root = document.getElementById("root");
 
 ReactDOM.render(
 	<StrictMode>
-		<App />
+		<TodoProvider>
+			<App />
+		</TodoProvider>
 	</StrictMode>,
 	root
 );
