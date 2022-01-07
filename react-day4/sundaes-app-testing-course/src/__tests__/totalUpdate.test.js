b0df44eb049b526f10a19c2d20f53a8a11876a84
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-07 17:33:52 +0530

    TE5-T197 added: context test for total value calculation

---
+++ react-day4/sundaes-app-testing-course/src/__tests__/totalUpdate.test.js
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Options from "../components/Options";
+import { OrderDetailsProvider } from "../contexts/order.context";
+
+test("should update scoop subtotal when scoops change", async () => {
+  render(<Options optionType={"scoops"} />, { wrapper: OrderDetailsProvider });
+
+  // start total with 0
+  const scoopTotal = screen.getByText("Scoops total: $", { exact: false });
+  expect(scoopTotal).toHaveTextContent("0.00");
+
+  // update vanilla to 1
+  const vanillaInput = await screen.findByRole("spinbutton", {
+    name: "Vanilla",
+  });
+  userEvent.clear(vanillaInput);
+  userEvent.type(vanillaInput, "1");
+  expect(scoopTotal).toHaveTextContent("2.00");
+
+  // update chocolate to 2
+  const chocolateInput = await screen.findByRole("spinbutton", {
+    name: "Chocolate",
+  });
+
+  userEvent.clear(chocolateInput);
+  userEvent.type(chocolateInput, "2");
+  expect(scoopTotal).toHaveTextContent("6.00");
+});
