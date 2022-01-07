ca4c1317bbd1d863d356c599bba10733535ac30b
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-07 10:23:23 +0530

    TE5-T197 refactored: getDate function

--- react-day4/practical-04/src/helpers/getDate.js
+++ react-day4/practical-04/src/helpers/getDate.js
@@ -1,24 +1,24 @@
 //  a helper function for getting current date (forms closure)
 export const getDate = (() => {
 	const monthNames = [
-		"January",
-		"February",
-		"March",
-		"April",
-		"May",
-		"June",
-		"July",
-		"August",
-		"September",
-		"October",
-		"November",
-		"December",
+		"JAN",
+		"FEB",
+		"MAR",
+		"APR",
+		"MAY",
+		"JUN",
+		"JUL",
+		"AUG",
+		"SEP",
+		"OCT",
+		"NOV",
+		"DEC",
 	];
 	return () => {
 		const dateObj = new Date();
 		const month = monthNames[dateObj.getMonth()];
 		const day = String(dateObj.getDate()).padStart(2, "0");
 		const year = dateObj.getFullYear();
-		return `${month} ${day}, ${year}`;
+		return { month, day, year };
 	};
 })();
