373257ba9d9082af23ee2a3093678d17ea589734
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-07 11:20:58 +0530

    TE5-T197 completed styles and functionality of AddTodo component

--- react-day4/practical-04/src/components/AddTodo/AddTodo.js
+++ react-day4/practical-04/src/components/AddTodo/AddTodo.js
@@ -1,8 +1,53 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { ADD_TODO } from "../../context/todo.actions";
+import { TodoContext } from "../../context/todo.context";
 import "./AddTodo.css";
 
 const AddTodo = () => {
-	return <div className="add-todo-parent">Add todo component</div>;
+	const [isInputVisible, setIsInputVisible] = useState(false);
+	const [todo, setTodo] = useState("");
+	const { dispatch } = useContext(TodoContext);
+
+	const handleToggle = () => {
+		setIsInputVisible((prevVisibility) => !prevVisibility);
+	};
+
+	const handleKeyInput = (e) => {
+		if (e.key === "Enter") {
+			if (todo !== "") {
+				dispatch({ type: ADD_TODO, payload: { title: todo } });
+				setTodo("");
+			} else {
+				alert("please add something before submitting");
+			}
+		}
+		if (e.key === "Escape") {
+			handleToggle();
+		}
+	};
+
+	return (
+		<div className="add-todo-parent">
+			{isInputVisible && (
+				<input
+					className="add-todo-input"
+					type={"text"}
+					placeholder="enter todo"
+					onKeyUp={handleKeyInput}
+					value={todo}
+					onChange={(e) => {
+						setTodo(e.target.value);
+					}}
+					autoFocus
+				/>
+			)}
+			{!isInputVisible && (
+				<button onClick={handleToggle} className="add-todo-button">
+					+
+				</button>
+			)}
+		</div>
+	);
 };
 
 export default AddTodo;
