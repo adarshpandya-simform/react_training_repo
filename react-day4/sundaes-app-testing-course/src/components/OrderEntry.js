9375c115fbf3a201ca00f505926077a40c0e63c2
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-10 10:12:36 +0530

    TE5-T197 updated: login in OrderEntry

--- react-day4/sundaes-app-testing-course/src/components/OrderEntry.js
+++ react-day4/sundaes-app-testing-course/src/components/OrderEntry.js
@@ -1,10 +1,14 @@
+import { useOrderDetails } from "../contexts/order.context";
 import Options from "./Options";
 
 const OrderEntry = () => {
+  const [orderDetails] = useOrderDetails();
   return (
     <div>
+      <h1>Design your sundae</h1>
       <Options optionType={"scoops"} />
       <Options optionType={"toppings"} />
+      <h2>Grand total: {orderDetails.totals.grandTotal}</h2>
     </div>
   );
 };
