2cf2c277129d8ef8d8626d786a756426d0066fc2
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-10 09:55:18 +0530

    TE5-T197 added form and checkbox

--- react-day4/sundaes-app-testing-course/src/components/ToppingOptions.js
+++ react-day4/sundaes-app-testing-course/src/components/ToppingOptions.js
@@ -1,6 +1,7 @@
 import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
 
-const ToppingOptions = ({ name, imagePath }) => {
+const ToppingOptions = ({ name, imagePath, updateItemCount }) => {
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
       <img
@@ -8,6 +9,15 @@ const ToppingOptions = ({ name, imagePath }) => {
         src={`http://localhost:3030/${imagePath}`}
         alt={`${name} topping`}
       />
+      <Form.Group controlId={`${name}-topping-checkbox`}>
+        <Form.Check
+          type="checkbox"
+          onChange={(e) => {
+            updateItemCount(name, e.target.checked ? 1 : 0);
+          }}
+          label={name}
+        ></Form.Check>
+      </Form.Group>
     </Col>
   );
 };
