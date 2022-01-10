16315fe2a598a49fc795049cf0be818f35d233f8
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-10 11:31:08 +0530

    TE5-T197 completed redux counter example

--- react-day6/redux-example/src/App.js
+++ react-day6/redux-example/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Counter from "./components/Counter";
+import { useSelector } from "react-redux";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>the value of counter {counter}</p>
+      <Counter />
     </div>
   );
 }
