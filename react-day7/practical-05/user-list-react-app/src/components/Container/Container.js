a7b731a6671cda7f27de3de69a63ddb791157548
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-11 12:48:24 +0530

    TE5-T197 added data fetching logic for dummy users

--- react-day7/practical-05/user-list-react-app/src/components/Container/Container.js
+++ react-day7/practical-05/user-list-react-app/src/components/Container/Container.js
@@ -1,11 +1,28 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import UserList from "../UserList/UserList";
 import UserProfile from "../UserProfile/UserProfile";
 import "./Container.css";
 
 const Container = () => {
+  const [userData, setUserData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await axios
+        .get("https://randomuser.me/api/?results=7")
+        .then((res) => setUserData(res.data.results))
+        .catch((error) => {
+          setUserData([]);
+          console.log(error);
+        });
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="container">
-      <UserList />
+      <UserList userData={userData} />
       <UserProfile />
     </div>
   );
