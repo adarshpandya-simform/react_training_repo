cb8b75f4c9a41818204e61f2dd8d22e4fec6fd17
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-11 15:07:35 +0530

    TE5-T197 separated logic and markup for first user

--- react-day7/practical-05/user-list-react-app/src/components/List/List.js
+++ react-day7/practical-05/user-list-react-app/src/components/List/List.js
@@ -1,4 +1,4 @@
-import { Trash2 } from "react-feather";
+import { Lock, Trash2 } from "react-feather";
 import "./List.css";
 
 const List = ({ userData, handleUserHover }) => {
@@ -6,42 +6,87 @@ const List = ({ userData, handleUserHover }) => {
     <div>
       {userData.length === 0 && <p>loading...</p>}
       {userData &&
-        userData.map((user) => (
-          <div key={user.phone} className="list">
-            <div
-              onMouseEnter={() => {
-                handleUserHover(user);
-              }}
-              className="user-details"
-            >
-              <div className="user-details-container">
-                <div className="user-profile-img">
-                  <img src={user.picture.large} alt="user-profile" />
+        userData.map((user, index) => (
+          <>
+            {index === 0 && (
+              <div key={user.phone} className="list">
+                <div
+                  onMouseEnter={() => {
+                    handleUserHover(user);
+                  }}
+                  onMouseLeave={() => {
+                    handleUserHover(null);
+                  }}
+                  className="user-details"
+                >
+                  <div className="user-details-container">
+                    <div className="user-profile-img">
+                      <img src={user.picture.large} alt="user-profile" />
+                    </div>
+                    <div className="user-profile-data">
+                      <span className="user-name">{`${user.name.first} ${user.name.last}`}</span>
+                      <span className="user-email">{user.email}</span>
+                    </div>
+                  </div>
                 </div>
-                <div className="user-profile-data">
-                  <span className="user-name">{`${user.name.first} ${user.name.last}`}</span>
-                  <span className="user-email">{user.email}</span>
+                <div className="user-info">
+                  <div className="user-status">
+                    <span className="first-user-status">Active</span>
+                  </div>
+                  <div className="user-access">
+                    <span className="first-user-access">Owner</span>
+                  </div>
+                  <div className="user-access-auth">
+                    <Lock />
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="user-info">
-              <div className="user-status">
-                <select className="user-status-select">
-                  <option className="user-status-select-option">
-                    Inactive
-                  </option>
-                </select>
-              </div>
-              <div className="user-access">
-                <select className="user-status-select">
-                  <option className="user-status-select-option">Manager</option>
-                </select>
-              </div>
-              <div className="user-access-auth">
-                <Trash2 />
-              </div>
-            </div>
-          </div>
+            )}
+            {index !== 0 && (
+              <>
+                <div key={user.phone} className="list">
+                  <div
+                    onMouseEnter={() => {
+                      handleUserHover(user);
+                    }}
+                    onMouseLeave={() => {
+                      handleUserHover(null);
+                    }}
+                    className="user-details"
+                  >
+                    <div className="user-details-container">
+                      <div className="user-profile-img">
+                        <img src={user.picture.large} alt="user-profile" />
+                      </div>
+                      <div className="user-profile-data">
+                        <span className="user-name">{`${user.name.first} ${user.name.last}`}</span>
+                        <span className="user-email">{user.email}</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="user-info">
+                    <div className="user-status">
+                      <select className="user-status-select">
+                        <option className="user-status-select-option">
+                          Inactive
+                        </option>
+                      </select>
+                    </div>
+                    <div className="user-access">
+                      <select className="user-status-select">
+                        <option className="user-status-select-option">
+                          Manager
+                        </option>
+                      </select>
+                    </div>
+                    <div className="user-access-auth">
+                      <Trash2 />
+                    </div>
+                  </div>
+                </div>
+              </>
+            )}
+          </>
         ))}
     </div>
   );
