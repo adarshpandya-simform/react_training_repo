9d020f451689043b175f69f78fd55d7d382c1b8b
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-11 10:33:56 +0530

    TE5-T197 added: ProfieCard in UserProfiel

--- react-day7/practical-05/user-list-react-app/src/components/UserProfile/UserProfile.js
+++ react-day7/practical-05/user-list-react-app/src/components/UserProfile/UserProfile.js
@@ -1,7 +1,12 @@
+import ProfileCard from "../ProfileCard/ProfileCard";
 import "./UserProfile.css";
 
 const UserProfile = () => {
-  return <div className="user-profile-parent">this is user-profile</div>;
+  return (
+    <div className="user-profile-parent">
+      <ProfileCard />
+    </div>
+  );
 };
 
 export default UserProfile;
