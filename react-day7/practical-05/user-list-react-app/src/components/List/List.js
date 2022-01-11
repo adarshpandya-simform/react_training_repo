3c9bff4b53833540ea10b99d17dec51c1e798d1a
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-11 13:31:28 +0530

    TE5-T197 refactored: loop through fetched data and empty msg markup in List component

--- react-day7/practical-05/user-list-react-app/src/components/List/List.js
+++ react-day7/practical-05/user-list-react-app/src/components/List/List.js
@@ -1,38 +1,48 @@
 import { Trash2 } from "react-feather";
 import "./List.css";
 
-const List = () => {
+const List = ({ userData, handleUserHover }) => {
   return (
-    <div className="list">
-      <div className="user-details">
-        <div className="user-details-container">
-          <div className="user-profile-img">
-            <img
-              src="https://randomuser.me/api/portraits/men/86.jpg"
-              alt="user-profile"
-            />
+    <div>
+      {userData.length === 0 && <p>loading...</p>}
+      {userData &&
+        userData.map((user) => (
+          <div key={user.phone} className="list">
+            <div
+              onMouseEnter={() => {
+                handleUserHover(user);
+              }}
+              className="user-details"
+            >
+              <div className="user-details-container">
+                <div className="user-profile-img">
+                  <img src={user.picture.large} alt="user-profile" />
+                </div>
+                <div className="user-profile-data">
+                  <span className="user-name">{`${user.name.first} ${user.name.last}`}</span>
+                  <span className="user-email">{user.email}</span>
+                </div>
+              </div>
+            </div>
+            <div className="user-info">
+              <div className="user-status">
+                <select className="user-status-select">
+                  <option className="user-status-select-option">
+                    Inactive
+                  </option>
+                </select>
+              </div>
+              <div className="user-access">
+                <select className="user-status-select">
+                  <option className="user-status-select-option">Manager</option>
+                </select>
+              </div>
+              <div className="user-access-auth">
+                <Trash2 />
+              </div>
+            </div>
           </div>
-          <div className="user-profile-data">
-            <span className="user-name">Jana Strassmann</span>
-            <span className="user-email">batz.mortimer@yahoo.com</span>
-          </div>
-        </div>
-      </div>
-      <div className="user-info">
-        <div className="user-status">
-          <select className="user-status-select">
-            <option className="user-status-select-option">Inactive</option>
-          </select>
-        </div>
-        <div className="user-access">
-          <select className="user-status-select">
-            <option className="user-status-select-option">Manager</option>
-          </select>
-        </div>
-        <div className="user-access-auth">
-          <Trash2 />
-        </div>
-      </div>
+        ))}
     </div>
   );
 };
