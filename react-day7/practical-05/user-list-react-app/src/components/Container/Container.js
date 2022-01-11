80fcbee85c09fbb5c5ddf7e164132c72870817ea
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-11 10:22:59 +0530

    TE5-T197 added: UserList and UserProfile component in Container

--- react-day7/practical-05/user-list-react-app/src/components/Container/Container.js
+++ react-day7/practical-05/user-list-react-app/src/components/Container/Container.js
@@ -1,7 +1,14 @@
+import UserList from "../UserList/UserList";
+import UserProfile from "../UserProfile/UserProfile";
 import "./Container.css";
 
 const Container = () => {
-  return <div className="container">this is container</div>;
+  return (
+    <div className="container">
+      <UserList />
+      <UserProfile />
+    </div>
+  );
 };
 
 export default Container;
