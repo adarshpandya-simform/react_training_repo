8ffc353c22e13d89450370190b768514df2bd23e
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-11 10:23:23 +0530

    TE5-T197 added: UserList and UserProfile component with basic markup

---
+++ react-day7/practical-05/user-list-react-app/src/components/UserList/UserList.js
@@ -0,0 +1,7 @@
+import "./UserList.css";
+
+const UserList = () => {
+  return <div className="user-list-parent">the list component</div>;
+};
+
+export default UserList;
---
+++ react-day7/practical-05/user-list-react-app/src/components/UserProfile/UserProfile.js
@@ -0,0 +1,7 @@
+import "./UserProfile.css";
+
+const UserProfile = () => {
+  return <div className="user-profile-parent">this is user-profile</div>;
+};
+
+export default UserProfile;
