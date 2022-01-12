c2b14cae92091c03c29ba097bd4a5a64e54b78bf
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-12 10:49:35 +0530

    TE5-T197 added: a button with handler in OrderEntry component and accepted setOrderPhase as prop

--- react-day4/sundaes-app-testing-course/src/components/OrderEntry.js
+++ react-day4/sundaes-app-testing-course/src/components/OrderEntry.js
@@ -1,7 +1,8 @@
+import Button from "react-bootstrap/Button";
 import { useOrderDetails } from "../contexts/order.context";
 import Options from "./Options";
 
-const OrderEntry = () => {
+const OrderEntry = ({ setOrderPhase }) => {
   const [orderDetails] = useOrderDetails();
   return (
     <div>
@@ -9,6 +10,7 @@ const OrderEntry = () => {
       <Options optionType={"scoops"} />
       <Options optionType={"toppings"} />
       <h2>Grand total: {orderDetails.totals.grandTotal}</h2>
+      <Button onClick={() => setOrderPhase("review")}>Order Sundae!</Button>
     </div>
   );
 };
