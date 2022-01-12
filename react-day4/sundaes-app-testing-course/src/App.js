3fa278adb7fef146f7ef9a2e2a72e95ed3852675
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-12 10:42:14 +0530

    TE5-T197 fixed: structure of App component with OrderContextProvider

--- react-day4/sundaes-app-testing-course/src/App.js
+++ react-day4/sundaes-app-testing-course/src/App.js
@@ -1,15 +1,31 @@
 import "./App.css";
-// import Options from "./components/Options";
 import OrderEntry from "./components/OrderEntry";
-import SummaryForm from "./components/SummaryForm";
+import OrderSummary from "./components/OrderSummary";
 import { OrderDetailsProvider } from "./contexts/order.context";
+import { useState } from "react";
+import OrderConfirmation from "./components/OrderConfirmation";
 
 function App() {
+  const [orderPhase, setOrderPhase] = useState("inProgress");
+  let Component = OrderEntry;
+
+  switch (orderPhase) {
+    case "inProgress":
+      Component = OrderEntry;
+      break;
+    case "review":
+      Component = OrderSummary;
+      break;
+    case "completed":
+      Component = OrderConfirmation;
+      break;
+    default:
+  }
+
   return (
     <div className="container">
       <OrderDetailsProvider>
-        <OrderEntry />
-        <SummaryForm />
+        <Component setOrderPhase={setOrderPhase} />
       </OrderDetailsProvider>
     </div>
   );
