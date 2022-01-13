1dfd3cad5d9edba477971fc5382eaed6db40db9a
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-13 09:58:20 +0530

    TE5-T197 added: custom hook

--- react-day8/redux-practise/src/store/counter.helper.js
+++ react-day8/redux-practise/src/store/counter.helper.js
@@ -1,3 +1,4 @@
+import { useDispatch, useSelector } from "react-redux";
 import { DECREMENT, INCREMENT, RESET } from "./counter.actions";
 
 // helper functions to dispatch actions
@@ -11,4 +12,23 @@ export const decrementCount = () => {
 
 export const resetCount = () => {
   return { type: RESET };
+};
+
+// creating a custom hook for counter
+export const useCounter = () => {
+  const count = useSelector((state) => state.count);
+  const dispatch = useDispatch();
+
+  return {
+    count,
+    increment: () => {
+      dispatch(incrementCount());
+    },
+    decrement: () => {
+      dispatch(decrementCount());
+    },
+    reset: () => {
+      dispatch(resetCount());
+    },
+  };
 };
