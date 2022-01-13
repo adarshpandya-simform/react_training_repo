107a5b27d95c446f7db0e41af16a3ed9897d11bb
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-13 12:52:53 +0530

    TE5-T197 combined reducers, added: thunk

---
+++ react-day8/redux-practise/src/store/store.js
@@ -0,0 +1,25 @@
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import { reducer as counterReducer } from "./counter.reducer";
+import { reducer as todoReducer } from "./todo.reducer";
+import { logger } from "../middlewares/logger";
+import { crashReporter } from "../middlewares/crashReport";
+import thunk from "redux-thunk";
+
+// combining reducers
+const rootReducer = combineReducers({
+  counter: counterReducer,
+  todos: todoReducer,
+});
+
+// creating store for counter
+export const store = createStore(
+  rootReducer,
+  applyMiddleware(logger, crashReporter, thunk)
+);
+
+// subscribed to state updates
+console.log(
+  store.subscribe(() => {
+    console.log(store.getState());
+  })
+);
