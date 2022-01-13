2839123b6d7e5e8a6740f7e16a90a2b8a27544b8
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-13 14:18:28 +0530

    TE5-T197 refactored: added thunk functions in hook, removed passed dispatcher

--- react-day8/redux-practise/src/store/todos.helper.js
+++ react-day8/redux-practise/src/store/todos.helper.js
@@ -1,7 +1,40 @@
 import { useDispatch, useSelector } from "react-redux";
+import { ADD_TODO, LOAD_TODO, SET_TODO } from "./todo.actions";
+import { addTodoThunk, fetchTodos } from "./todo.thunk";
 
+// todo action helper function
+export const loadTodos = () => {
+  return {
+    type: LOAD_TODO,
+  };
+};
+
+export const setTodos = (todos) => {
+  return {
+    type: SET_TODO,
+    payload: todos,
+  };
+};
+
+export const addTodo = (todo) => {
+  return {
+    type: ADD_TODO,
+    payload: todo,
+  };
+};
+
+// custom hook for todos
 export const useTodos = () => {
   const { todos, loading } = useSelector((state) => state.todos);
   const dispatch = useDispatch();
-  return { todos, loading, dispatch };
+
+  const fetchTodosThunk = () => {
+    dispatch(fetchTodos());
+  };
+
+  const addNewTodoThunk = (title) => {
+    dispatch(addTodoThunk(title));
+  };
+
+  return { todos, loading, fetchTodosThunk, addNewTodoThunk };
 };
