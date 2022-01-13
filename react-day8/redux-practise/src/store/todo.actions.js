d134fb517b875d43903b6ee1ab39712bc34901fe
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-13 15:02:09 +0530

    TE5-T197 added: CHECK_TODO action

--- react-day8/redux-practise/src/store/todo.actions.js
+++ react-day8/redux-practise/src/store/todo.actions.js
@@ -3,3 +3,4 @@ export const LOAD_TODO = "LOAD_TODO";
 export const SET_TODO = "SET_TODO";
 export const ADD_TODO = "ADD_TODO";
 export const DELETE_TODO = "DELETE_TODO";
+export const CHECk_TODO = "CHECK_TODO";
