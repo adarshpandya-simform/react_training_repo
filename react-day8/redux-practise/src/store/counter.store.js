cdfab8cdcbd9de675c8a0c81d48837801befc632
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-13 10:31:15 +0530

    TE5-T197 applied middleware with applyMiddleware function

--- react-day8/redux-practise/src/store/counter.store.js
+++ react-day8/redux-practise/src/store/counter.store.js
@@ -1,5 +1,10 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import { reducer } from "./counter.reducer";
+import { logger } from "../middlewares/logger";
+import { crashReporter } from "../middlewares/crashReport";
 
 // creating store for counter
-export const store = createStore(reducer);
+export const store = createStore(
+  reducer,
+  applyMiddleware(logger, crashReporter)
+);
