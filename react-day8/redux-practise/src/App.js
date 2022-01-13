d3c50207326df68855f458c0881cd89c81a2ed89
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-13 10:07:53 +0530

    TE5-T197 rendered both Counter and EmptyComponent in App

--- react-day8/redux-practise/src/App.js
+++ react-day8/redux-practise/src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import Counter from "./components/Counter/Counter";
+import EmptyComponent from "./components/EmptyComponent/EmptyComponent";
 import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div>
       <Header />
+      <Counter />
+      <EmptyComponent />
     </div>
   );
 }
