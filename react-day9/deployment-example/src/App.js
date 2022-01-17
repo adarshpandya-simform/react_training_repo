6833790dfb48bc14323c860019307c3543e79832
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-17 16:49:35 +0530

    TE5-T197 added: counter functionality

--- react-day9/deployment-example/src/App.js
+++ react-day9/deployment-example/src/App.js
@@ -1,9 +1,26 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div>
       <p>this is deployed site!!!</p>
+      <h5>current count {count}</h5>
+      <button
+        onClick={() => {
+          setCount((prevCount) => prevCount + 1);
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          setCount((prevCount) => prevCount - 1);
+        }}
+      >
+        -
+      </button>
     </div>
   );
 }
