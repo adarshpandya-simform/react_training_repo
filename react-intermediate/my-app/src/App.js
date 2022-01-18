144dd4d329a2524301ddc5f80ec2b54a53fbdf56
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-18 12:36:31 +0530

    TE5-T197 added: Home component in App

--- react-intermediate/my-app/src/App.js
+++ react-intermediate/my-app/src/App.js
@@ -1,5 +1,11 @@
+import Home from "./components/Home/Home";
+
 function App() {
-  return <div>sample react app</div>;
+  return (
+    <div>
+      <Home />
+    </div>
+  );
 }
 
 export default App;
