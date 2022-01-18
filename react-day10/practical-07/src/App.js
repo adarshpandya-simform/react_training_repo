f52675edb8d44fb0f78f5601d604e56f5a6296f0
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-18 10:03:54 +0530

    TE5-T197 added: comments

--- react-day10/practical-07/src/App.js
+++ react-day10/practical-07/src/App.js
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div>
+      {/* accessing the env mode */}
       <p>this is {process.env.NODE_ENV} mode</p>
+      {/* accessing the env vars based on environment */}
       <p>here's the secret msg: {process.env.REACT_APP_SECRET_MSG} </p>
     </div>
   );
