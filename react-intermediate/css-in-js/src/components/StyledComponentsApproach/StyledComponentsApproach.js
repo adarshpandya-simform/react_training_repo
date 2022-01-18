f3abeb6b9aef5fbbffe1f498f7cdab19cfa267e9
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-18 15:38:51 +0530

    TE5-T197 created: StyledComponentApproachComponent

--- react-intermediate/css-in-js/src/components/StyledComponentsApproach/StyledComponentsApproach.js
+++ react-intermediate/css-in-js/src/components/StyledComponentsApproach/StyledComponentsApproach.js
@@ -1,5 +1,35 @@
-const StyledComponentsApproach = () => {
-  return <div></div>;
+import styled, { css } from "styled-components";
+
+const StyledComponentsApproach = ({ isActive, setActive }) => {
+  const StyledButton = styled.button`
+    color: white;
+    background: purple;
+    border: none;
+    border-radius: 4px;
+    font-size: 15px;
+    text-transform: uppercase;
+    padding: 10px 30px;
+
+    ${(props) =>
+      props.active &&
+      css`
+        background: yellow;
+        color: black !important;
+      `}
+  `;
+
+  return (
+    <div>
+      <StyledButton
+        active={isActive}
+        onClick={() => {
+          setActive((prevState) => !prevState);
+        }}
+      >
+        {isActive ? "deactivate" : "activate"}
+      </StyledButton>
+    </div>
+  );
 };
 
 export default StyledComponentsApproach;
