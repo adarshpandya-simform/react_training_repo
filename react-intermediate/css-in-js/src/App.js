fa711d617b5848ed8d82900c79241113fcfb0417
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-18 15:02:54 +0530

    TE5-T197 added: TraditionalApproach

--- react-intermediate/css-in-js/src/App.js
+++ react-intermediate/css-in-js/src/App.js
@@ -1,8 +1,21 @@
+import { useState } from "react";
+import TraditionalApproach from "./components/TraditionalApproach/TraditionalApproach";
 import "./App.css";
 
 // CSS in JS with different approaches
 function App() {
-  return <div className="App">hello </div>;
+  const [active1, setActive1] = useState(false);
+  const [active2, setActive2] = useState(false);
+  const [active3, setActive3] = useState(false);
+  const [active4, setActive4] = useState(false);
+  const [active5, setActive5] = useState(false);
+  const [active6, setActive6] = useState(false);
+  const [active7, setActive7] = useState(false);
+  return (
+    <div className="App">
+      <TraditionalApproach isActive={active1} setActive={setActive1} />
+    </div>
+  );
 }
 
 export default App;
