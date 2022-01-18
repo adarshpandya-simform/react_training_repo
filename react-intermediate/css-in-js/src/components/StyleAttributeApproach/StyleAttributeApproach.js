e19bec6c43166313cb2fa58091cb1032abc547b5
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-18 15:11:01 +0530

    TE5-T197 created: StyleAttributeAppraoch component

---
+++ react-intermediate/css-in-js/src/components/StyleAttributeApproach/StyleAttributeApproach.js
@@ -0,0 +1,25 @@
+import "./StyleAttributeApproach.css";
+
+const StyleAttributeApproach = ({ isActive, setActive }) => {
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setActive((prevState) => !prevState);
+        }}
+        className="button"
+        style={
+          isActive
+            ? {
+                background: "rgb(31, 133, 0)",
+              }
+            : {}
+        }
+      >
+        {isActive ? "deactivate" : "activate"}
+      </button>
+    </div>
+  );
+};
+
+export default StyleAttributeApproach;
