8a6b21db79892a9374fc214d9ea12a1e2edf255b
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-18 15:29:55 +0530

    TE5-T197 created: ClassnameApproach component

--- react-intermediate/css-in-js/src/components/ClassnameApproach/ClassnameApproach.js
+++ react-intermediate/css-in-js/src/components/ClassnameApproach/ClassnameApproach.js
@@ -1,5 +1,18 @@
-const ClassnameApproach = () => {
-  return <div></div>;
+import classnames from "classnames";
+
+const ClassnameApproach = ({ isActive, setActive }) => {
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setActive((prevState) => !prevState);
+        }}
+        className={classnames("button", { active: isActive })}
+      >
+        {isActive ? "deactivate" : "activate"}
+      </button>
+    </div>
+  );
 };
 
 export default ClassnameApproach;
