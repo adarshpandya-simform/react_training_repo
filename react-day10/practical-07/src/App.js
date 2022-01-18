72803dded1347c688d4e26299a3b50cfc7a7cfde
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-18 09:55:05 +0530

    TE5-T197 added and accessed env in app

--- react-day10/practical-07/src/App.js
+++ react-day10/practical-07/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
       <p>this is {process.env.NODE_ENV} mode</p>
-      <p>here's the secret msg: </p>
+      <p>here's the secret msg: {process.env.REACT_APP_SECRET_MSG} </p>
     </div>
   );
 }
