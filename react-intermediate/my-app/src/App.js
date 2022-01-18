8d73a2f1a338cceaf21aa382bb86b78cf3867b6f
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-18 13:50:10 +0530

    TE5-T197 added: emotion, used: Spinner in App js for Suspense fallback

--- react-intermediate/my-app/src/App.js
+++ react-intermediate/my-app/src/App.js
@@ -1,6 +1,7 @@
 import Home from "./components/Home/Home";
 import { Routes, Route, Link } from "react-router-dom";
 import { lazy, Suspense } from "react";
+import PuffLoader from "react-spinners/PuffLoader";
 
 const Dashboard = lazy(() => import("./components/Dashboard/Dashboard"));
 const EmojiPicker = lazy(() => import("./components/EmojiPicker/EmojiPicker"));
@@ -14,7 +15,9 @@ function App() {
       <br />
       <Link to={"/dashboard"}>Dashboard</Link>
       <br />
-      <Suspense fallback={<p>loading...</p>}>
+      <Suspense
+        fallback={<PuffLoader color={"#262626"} loading={true} size={100} />}
+      >
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/emoji" element={<EmojiPicker />} />
