36e39dfbbe659ddd7c2e384ed742ab123b9ffa8e
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-18 12:58:02 +0530

    TE5-T197 added: Links for paths

--- react-intermediate/my-app/src/App.js
+++ react-intermediate/my-app/src/App.js
@@ -1,10 +1,17 @@
 import Dashboard from "./components/Dashboard/Dashboard";
 import EmojiPicker from "./components/EmojiPicker/EmojiPicker";
 import Home from "./components/Home/Home";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div>
+      <Link to={"/"}>Home</Link>
+      <br />
+      <Link to={"/emoji"}>Emoji Picker</Link>
+      <br />
+      <Link to={"/dashboard"}>Dashboard</Link>
+      <br />
       <Home />
       <EmojiPicker />
       <Dashboard />
