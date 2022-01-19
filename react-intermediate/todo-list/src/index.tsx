bd0eae0fc6a95e1eeea32b8d0730827b650e0187
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-19 13:27:25 +0530

    TE5-T197 wrapped: App component inside TodoProvider

--- react-intermediate/todo-list/src/index.tsx
+++ react-intermediate/todo-list/src/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { TodoProvider } from "./context/todo.context";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TodoProvider>
+      <App />
+    </TodoProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
