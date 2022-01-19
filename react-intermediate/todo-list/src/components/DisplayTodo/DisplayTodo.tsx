f27fa7fa561781e22142d1c89f95ddd06c46d309
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-19 14:31:02 +0530

    TE5-T197 modified: markup and added: css for styling in DisplayTodo component

--- react-intermediate/todo-list/src/components/DisplayTodo/DisplayTodo.tsx
+++ react-intermediate/todo-list/src/components/DisplayTodo/DisplayTodo.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
 import { useTodos } from "../../context";
+import "./DisplayTodo.css";
 
 const DisplayTodo: FC = () => {
   const { deleteTodo, checkTodo, todos } = useTodos();
@@ -13,18 +14,22 @@ const DisplayTodo: FC = () => {
   };
 
   return (
-    <div>
+    <div className="display-container">
       {todos.length === 0 && <p>yayy! no todos</p>}
       {todos.map((todo) => (
-        <div key={todo.id}>
-          <input
-            type={"checkbox"}
-            checked={todo.completed}
-            onChange={(e) => {
-              handleCheck(todo.id, e.target.checked);
-            }}
-          />
-          <span>{todo.title}</span>
+        <div className="todo" key={todo.id}>
+          <div className="todo-info">
+            <input
+              type={"checkbox"}
+              checked={todo.completed}
+              onChange={(e) => {
+                handleCheck(todo.id, e.target.checked);
+              }}
+            />
+            <span className={todo.completed ? "completed" : ""}>
+              {todo.title}
+            </span>
+          </div>
           <button
             onClick={() => {
               handleDelete(todo.id);
