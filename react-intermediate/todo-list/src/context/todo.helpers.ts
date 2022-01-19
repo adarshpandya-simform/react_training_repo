064e098b6571b7fe02b95350dc701a37f68449ba
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-19 12:16:28 +0530

    TE5-T197 created: a custom hook for todo-context as helper

---
+++ react-intermediate/todo-list/src/context/todo.helpers.ts
@@ -0,0 +1,55 @@
+import { useContext } from "react";
+import { ADD_TODO, CHECK_TODO, DELETE_TODO } from "./todo.action";
+import { Todo, TodoContext } from "./todo.context";
+
+export const addTodoAction = (
+  title: string
+): { type: string; payload: string } => {
+  return {
+    type: ADD_TODO,
+    payload: title,
+  };
+};
+
+export const deleteTodoAction = (
+  id: string
+): { type: string; payload: string } => {
+  return {
+    type: DELETE_TODO,
+    payload: id,
+  };
+};
+
+export const checkTodoAcion = (
+  id: string,
+  completed: boolean
+): { type: string; payload: { id: string; completed: boolean } } => {
+  return {
+    type: CHECK_TODO,
+    payload: { id, completed },
+  };
+};
+
+export const useTodos = (): {
+  todos: Array<Todo>;
+  addTodo: (title: string) => void;
+  deleteTodo: (id: string) => void;
+  checkTodo: (id: string, completed: boolean) => void;
+} => {
+  const { state, dispatch } = useContext(TodoContext);
+  let todos: Array<Todo> = state.todos;
+
+  const addTodo = (title: string): void => {
+    dispatch(addTodoAction(title));
+  };
+
+  const deleteTodo = (id: string): void => {
+    dispatch(deleteTodoAction(id));
+  };
+
+  const checkTodo = (id: string, completed: boolean) => {
+    dispatch(checkTodoAcion(id, completed));
+  };
+
+  return { todos, addTodo, deleteTodo, checkTodo };
+};
