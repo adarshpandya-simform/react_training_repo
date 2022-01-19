592405205b918d5177e20c342d5a23e5a692f6cc
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-19 16:55:18 +0530

    TE5-T197 passed: counterStore as prop in App component

--- react-state-management/mobx-counter/src/index.js
+++ react-state-management/mobx-counter/src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { counterStore } from "./store/counter-store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App counterStore={counterStore} />
   </React.StrictMode>,
   document.getElementById("root")
 );
