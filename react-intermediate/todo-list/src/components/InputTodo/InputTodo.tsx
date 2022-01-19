d079b9145cad2fc06af9b5a31a5d1188984edb2c
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-19 13:29:54 +0530

    TE5-T197 created: InputTodo component with functionality and markup

---
+++ react-intermediate/todo-list/src/components/InputTodo/InputTodo.tsx
@@ -0,0 +1,42 @@
+import React, { FC, useState } from "react";
+import { useTodos } from "../../context";
+import "./InputTodo.css";
+
+const InputTodo: FC = () => {
+  const [title, setTitle] = useState<string>("");
+  const { addTodo } = useTodos();
+
+  const handleSubmit = (): void => {
+    if (title !== "") {
+      addTodo(title);
+      setTitle("");
+    } else {
+      alert("please add todo before submitting");
+    }
+  };
+
+  const handleKeyPress = (key: string): void => {
+    if (key === "Enter") {
+      handleSubmit();
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type={"text"}
+        placeholder="Enter todo"
+        value={title}
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
+        onKeyUp={(e) => {
+          handleKeyPress(e.key);
+        }}
+      />
+      <button onClick={handleSubmit}>add todo</button>
+    </div>
+  );
+};
+
+export default InputTodo;
