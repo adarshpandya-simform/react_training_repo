0b30144e4febd30397f998006b0037ac8fec0945
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-19 16:21:06 +0530

    TE5-T197 created: basic timer with mobx store and actions

--- react-state-management/mobx-timer/src/App.js
+++ react-state-management/mobx-timer/src/App.js
@@ -1,7 +1,25 @@
+import { observer } from "mobx-react";
+import { useEffect } from "react";
 import "./App.css";
 
-function App() {
-  return <div>Hello world</div>;
-}
+// using observer from mobx-react to observe state
+const App = observer(({ timer }) => {
+  useEffect(() => {
+    setInterval(() => {
+      timer.increase();
+    }, 1000);
+  }, [timer]);
+
+  // calling reset method in jsx
+  return (
+    <p
+      onClick={() => {
+        timer.reset();
+      }}
+    >
+      seconds passed: {timer.secondPassed}
+    </p>
+  );
+});
 
 export default App;
