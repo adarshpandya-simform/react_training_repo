9f48d19b3619c3c5feda29f4af6e9335398d3445
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-19 16:20:23 +0530

    TE5-T197 passed store(timer) in App component as prop

--- react-state-management/mobx-timer/src/index.js
+++ react-state-management/mobx-timer/src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import timer from "./store";
 
+// passing timer(store) in App component
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App timer={timer} />
   </React.StrictMode>,
   document.getElementById("root")
 );
