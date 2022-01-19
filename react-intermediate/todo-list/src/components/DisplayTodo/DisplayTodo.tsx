7dfb41ddd298e5fd60844bd3fe2bee3ac4fd5c75
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-19 13:29:29 +0530

    TE5-T197 created: DisplayTodo component with functionality and markup

---
+++ react-intermediate/todo-list/src/components/DisplayTodo/DisplayTodo.tsx
@@ -0,0 +1,42 @@
+import React, { FC } from "react";
+import { useTodos } from "../../context";
+import "./DisplayTodo.css";
+
+const DisplayTodo: FC = () => {
+  const { deleteTodo, checkTodo, todos } = useTodos();
+
+  const handleDelete = (id: string): void => {
+    deleteTodo(id);
+  };
+
+  const handleCheck = (id: string, completed: boolean): void => {
+    checkTodo(id, completed);
+  };
+
+  return (
+    <div>
+      {todos.length === 0 && <p>yayy! no todos</p>}
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          <input
+            type={"checkbox"}
+            checked={todo.completed}
+            onChange={(e) => {
+              handleCheck(todo.id, e.target.checked);
+            }}
+          />
+          <span>{todo.title}</span>
+          <button
+            onClick={() => {
+              handleDelete(todo.id);
+            }}
+          >
+            delete
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default DisplayTodo;
