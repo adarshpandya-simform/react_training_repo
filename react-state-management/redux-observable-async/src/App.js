f1f652aab68e837873664aeddfe7d7ab63a0b925
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-20 17:12:55 +0530

    TE5-T197 rendered: FetchButton and DisplayTodo in App component

--- react-state-management/redux-observable-async/src/App.js
+++ react-state-management/redux-observable-async/src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import DisplayTodos from "./components/DisplayTodos";
+import FetchButton from "./components/FetchButton";
 
 const App = () => {
-  return <div>hello world</div>;
+  return (
+    <div>
+      <FetchButton />
+      <DisplayTodos />
+    </div>
+  );
 };
 
 export default App;
