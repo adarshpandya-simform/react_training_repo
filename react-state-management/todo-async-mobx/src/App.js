829001baa95e1a569c1fe9c5df60aa96411ef762
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-20 11:49:38 +0530

    TE5-T197 rendered: InputTodo and DisplayTodo component with loading state

--- react-state-management/todo-async-mobx/src/App.js
+++ react-state-management/todo-async-mobx/src/App.js
@@ -1,7 +1,34 @@
+import { useEffect } from "react";
 import "./App.css";
+import { useTodos } from "./context/todo.context";
+import { useObserver } from "mobx-react";
+import InputTodo from "./components/InputTodo/InputTodo";
+import DisplayTodo from "./components/DisplayTodo/DisplayTodo";
 
 function App() {
-  return <div>hello</div>;
+  const todoStore = useTodos();
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      await todoStore.loadTodos();
+    };
+    fetchTodos();
+  }, [todoStore]);
+
+  return useObserver(() => {
+    const { isLoading } = todoStore;
+    return (
+      <div>
+        {isLoading && <p>fetching todos</p>}
+        {!isLoading && (
+          <>
+            <InputTodo />
+            <DisplayTodo />
+          </>
+        )}
+      </div>
+    );
+  });
 }
 
 export default App;
