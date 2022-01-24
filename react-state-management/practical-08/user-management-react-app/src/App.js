2bd043a9da47b08f18a64d9fc18afaf374a56d5f
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-24 14:56:48 +0530

    TE5-T197 modified: '/' route

--- react-state-management/practical-08/user-management-react-app/src/App.js
+++ react-state-management/practical-08/user-management-react-app/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { HomePage, SignupPage } from "./pages";
 
 const App = () => {
 	return (
 		<Routes>
-			<Route path="/" element={<SignupPage />} />
+			<Route path="/" element={<Navigate to={"/signup"} />} />
 			<Route path="/home" element={<HomePage />} />
 			<Route path="/signup" element={<SignupPage />} />
 		</Routes>
