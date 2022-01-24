d445529568261f214f2761580ca4483568093c2b
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-24 17:04:03 +0530

    TE5-T197 added: conditional navigation in SignupPage to navigate to /home if logged in

--- react-state-management/practical-08/user-management-react-app/src/pages/SignupPage/SignupPage.js
+++ react-state-management/practical-08/user-management-react-app/src/pages/SignupPage/SignupPage.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./SignupPage.css";
 import { SignupForm, WorkingWoman } from "../../components";
+import { useNavigate } from "react-router-dom";
+import { useUser } from "../../hooks/useUser";
 
 const SignupPage = () => {
+	const navigate = useNavigate();
+	const { getUserLoggedInStatus } = useUser();
+
+	useEffect(() => {
+		if (getUserLoggedInStatus) {
+			navigate("/home");
+		}
+	}, []);
+
 	return (
 		<div className="signup-page-container">
 			<SignupForm />
