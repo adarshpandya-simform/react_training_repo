dda5bad58091ef96de03d82aa10b74ec67e95961
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-24 17:07:57 +0530

    TE5-T197 exported UserProfile component from index

--- react-state-management/practical-08/user-management-react-app/src/components/index.js
+++ react-state-management/practical-08/user-management-react-app/src/components/index.js
@@ -1,2 +1,3 @@
 export { default as SignupForm } from "./SignupForm/SignupForm";
 export { default as WorkingWoman } from "./WorkingWoman/WorkingWoman";
+export { default as UserProfile } from "./UserProfile/UserProfile";
