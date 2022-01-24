0133fa33c4d03ef868b6462e86c489fe07bf00c4
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-24 10:30:46 +0530

    TE5-T197 rendered: pages in App component with Router

--- react-state-management/practical-08/user-management-react-app/src/App.js
+++ react-state-management/practical-08/user-management-react-app/src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { HomePage, SignupPage } from "./pages";
 
 const App = () => {
-	return <div>hello world</div>;
+	return (
+		<Routes>
+			<Route path="/" element={<SignupPage />} />
+			<Route path="/home" element={<HomePage />} />
+			<Route path="/signup" element={<SignupPage />} />
+		</Routes>
+	);
 };
 
 export default App;
