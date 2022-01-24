527a6dc505d2d514610c18db5e073c5a874d22b9
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-24 17:09:49 +0530

    TE5-T197 rendered: UserProfile component in HomePage

--- react-state-management/practical-08/user-management-react-app/src/pages/HomePage/HomePage.js
+++ react-state-management/practical-08/user-management-react-app/src/pages/HomePage/HomePage.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { UserProfile } from "../../components/";
 import "./HomePage.css";
 
 const HomePage = () => {
-	return <div className="home-page-container">home page</div>;
+	return (
+		<div className="home-page-container">
+			<UserProfile />
+		</div>
+	);
 };
 
 export default HomePage;
