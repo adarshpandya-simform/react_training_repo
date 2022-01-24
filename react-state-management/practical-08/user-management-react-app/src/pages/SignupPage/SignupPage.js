c1c7c13c9872dc9c4ad6d4dcab89d44f39d5b497
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-24 11:05:50 +0530

    TE5-T197 rendered: SignupForm and WorkingWoman components, added: css for styling

--- react-state-management/practical-08/user-management-react-app/src/pages/SignupPage/SignupPage.js
+++ react-state-management/practical-08/user-management-react-app/src/pages/SignupPage/SignupPage.js
@@ -1,7 +1,14 @@
 import React from "react";
+import "./SignupPage.css";
+import { SignupForm, WorkingWoman } from "../../components";
 
 const SignupPage = () => {
-	return <div>signup page</div>;
+	return (
+		<div className="signup-page-container">
+			<SignupForm />
+			<WorkingWoman />
+		</div>
+	);
 };
 
 export default SignupPage;
