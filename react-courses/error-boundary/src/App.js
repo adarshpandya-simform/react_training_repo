b843d59a4ad41ac9de1a633098eab3451ae17f54
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-25 16:58:21 +0530

    TE5-T197 added: ErrorBoundary on Counter component

--- react-courses/error-boundary/src/App.js
+++ react-courses/error-boundary/src/App.js
@@ -1,13 +1,26 @@
 import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import Counter from "./components/Counter";
+import About from "./components/About";
+import Home from "./components/Home";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 const App = () => {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
-      <Counter count={count} setCount={setCount} />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route
+        path="/counter"
+        element={
+          <ErrorBoundary>
+            <Counter count={count} setCount={setCount} />
+          </ErrorBoundary>
+        }
+      />
+      <Route path="/about" element={<About />} />
+    </Routes>
   );
 };
 
