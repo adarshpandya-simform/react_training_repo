0722f443930bd676389ea2421cc6cc62c2c93cb7
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-25 18:28:01 +0530

    TE5-T197 rendered: Home and Counter component in App component on respected Route

--- react-courses/react-error-boundary/src/App.js
+++ react-courses/react-error-boundary/src/App.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import Counter from "./components/Counter";
+import ErrorBoundaryComponent from "./components/ErrorBoundaryComponent";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
 
+// App component which passes count and setCount to Counter
 const App = () => {
-  return <div>hello world</div>;
+  const [count, setCount] = useState(0);
+
+  return (
+    // Router with Routes and path
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route
+        path="/counter"
+        element={
+          <ErrorBoundaryComponent onReset={() => setCount(0)}>
+            <Counter count={count} setCount={setCount} />
+          </ErrorBoundaryComponent>
+        }
+      />
+    </Routes>
+  );
 };
 
 export default App;
