86dd901a48a720395df48afecd828987ea59ac32
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-25 14:57:50 +0530

    TE5-T197 wrapped: App component in BrowserRouter for routing

--- react-courses/error-boundary/src/index.js
+++ react-courses/error-boundary/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import ErrorBoundary from "./components/ErrorBoundary";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <ErrorBoundary>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </ErrorBoundary>
   </React.StrictMode>,
   document.getElementById("root")
