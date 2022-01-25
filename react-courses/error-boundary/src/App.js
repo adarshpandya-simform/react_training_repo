a7c71fe88a0538b2e88cd956d6e7ee61995267fb
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-25 14:49:47 +0530

    TE5-T197 rendered Counter component, passed: count and setCount as prop

--- react-courses/error-boundary/src/App.js
+++ react-courses/error-boundary/src/App.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import Counter from "./components/Counter";
 
 const App = () => {
-	return <div></div>;
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <Counter count={count} setCount={setCount} />
+    </div>
+  );
 };
 
 export default App;
