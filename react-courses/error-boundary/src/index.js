7cdc6c95db34b4f0b409af726c271af196f1cc4c
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-25 16:47:19 +0530

    TE5-T197 removed: ErrorBoundary wrapper in index

--- react-courses/error-boundary/src/index.js
+++ react-courses/error-boundary/src/index.js
@@ -2,16 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import ErrorBoundary from "./components/ErrorBoundary";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <Router>
-        <App />
-      </Router>
-    </ErrorBoundary>
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
