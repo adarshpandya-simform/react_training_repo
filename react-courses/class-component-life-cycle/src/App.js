ed0b43b47b10dcc53259a94f059464aaa0d953fb
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-27 15:15:15 +0530

    TE5-T197 initialized: CRA app for class component life cycle practise

---
+++ react-courses/class-component-life-cycle/src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+// class components
+export default class App extends Component {
+  state = { todos: [] };
+  render() {
+    return <div>hello world</div>;
+  }
+}
