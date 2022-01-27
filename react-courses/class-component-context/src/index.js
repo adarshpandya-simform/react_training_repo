eba111dec4ac84deb457a963e47536b8f59ef89e
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-27 16:18:40 +0530

    TE5-T197 wrapped: App component inside CounterProvider

--- react-courses/class-component-context/src/index.js
+++ react-courses/class-component-context/src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { CounterProvider } from "./contexts/CounterContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CounterProvider>
+      <App />
+    </CounterProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
