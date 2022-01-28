dfabba1b8fdafc55df9356ce5c4181e02ba2f8aa
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-28 16:12:31 +0530

    TE5-T197 rendered: AddUser and Users component and used useDate and useTime hook to show info

--- react-courses-2/custom-hooks/src/App.js
+++ react-courses-2/custom-hooks/src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import AddUser from "./components/AddUser";
+import Users from "./components/Users";
+import { useDate } from "./hooks/useDate";
+import { useTime } from "./hooks/useTime";
 
 const App = () => {
-  return <div>hello world</div>;
+  const { date } = useDate("dd-mm-yy", "-");
+  const { date: date2 } = useDate("dd-mon-yyyy", " ");
+  const { time } = useTime();
+  return (
+    <div>
+      <p>time : {time}</p>
+      <p>Today's Date : {date}</p>
+      <p>Today's Date : {date2}</p>
+      <Users />
+      <AddUser />
+    </div>
+  );
 };
 
 export default App;
