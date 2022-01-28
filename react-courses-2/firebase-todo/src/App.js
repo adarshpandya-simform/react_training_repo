9f0a543ef7b56efdd3231a1afb41020d7da6e74a
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-28 19:08:07 +0530

    TE5-T197 rendered: DisplayTodo and AddTodo component and dispatched loadTodosAction

--- react-courses-2/firebase-todo/src/App.js
+++ react-courses-2/firebase-todo/src/App.js
@@ -1,7 +1,32 @@
-import React from "react";
+import { collection, onSnapshot, query } from "firebase/firestore";
+import React, { useEffect } from "react";
+import AddTodo from "./components/AddTodo";
+import DisplayTodos from "./components/DisplayTodos";
+import { db } from "./config/firebase.config";
+import { useTodos } from "./hooks/useTodos";
 
 const App = () => {
-  return <div>sample app</div>;
+  const { loadTodosAction } = useTodos();
+
+  useEffect(() => {
+    const myQuery = query(collection(db, "todos"));
+    const unsub = onSnapshot(myQuery, (querySnapshot) => {
+      let todosArray = [];
+      querySnapshot.forEach((doc) => {
+        todosArray.push({ ...doc.data(), doc_id: doc.id });
+      });
+      loadTodosAction(todosArray);
+    });
+    return () => unsub();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div>
+      <AddTodo />
+      <DisplayTodos />
+    </div>
+  );
 };
 
 export default App;
