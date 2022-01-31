fd676dc079de92471dcb82559843880c8332f379
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-31 12:48:22 +0530

    TE5-T197 added: Routes ,Suspense and fallback in App component

--- react-courses-2/routing-v6/src/App.js
+++ react-courses-2/routing-v6/src/App.js
@@ -1,7 +1,41 @@
-import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./components";
+import { lazy, Suspense } from "react";
 
+const TodoContainer = lazy(() =>
+  import("./components/TodoContainer/TodoContainer")
+);
+const About = lazy(() => import("./components/About/About"));
+const Contact = lazy(() => import("./components/Contact/Contact"));
+const TodoLayout = lazy(() => import("./pages/TodoLayout"));
+
+// App component with lazy loading,suspense and fallback
 const App = () => {
-  return <div>hello world</div>;
+  return (
+    // suspense with fallback
+    <Suspense fallback={<p>loading...</p>}>
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<TodoLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />}>
+            <Route
+              path="/contact/email"
+              element={<p>email: ap@simformsolutions.com</p>}
+            />
+            <Route
+              path="/contact/phone"
+              element={<p>phone: +91 - 9283940404</p>}
+            />
+          </Route>
+          <Route path="/todos" element={<TodoContainer />} />
+          {/* adding 404 route */}
+          <Route path="*" element={<p>404 not found</p>} />
+        </Route>
+      </Routes>
+    </Suspense>
+  );
 };
 
 export default App;
