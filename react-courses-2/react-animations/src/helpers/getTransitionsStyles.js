7c37edd7101f058e1693df453b300fea6d57e97a
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-01-31 17:47:54 +0530

    TE5-T197 added: helper function to get transition styles

---
+++ react-courses-2/react-animations/src/helpers/getTransitionsStyles.js
@@ -0,0 +1,6 @@
+export const getTransitionStyles = () => ({
+  entering: { opacity: 1, transform: "scale(1)" },
+  entered: { opacity: 1, transform: "scale(1)" },
+  exiting: { opacity: 0, transform: "scale(0)" },
+  exited: { opacity: 0, transform: "scale(0)" },
+});
