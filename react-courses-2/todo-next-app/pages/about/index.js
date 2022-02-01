645ca0770c55b8f895b347e8dfc5d618100b457a
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-02-01 16:26:30 +0530

    TE5-T197 created: about page @ /about

---
+++ react-courses-2/todo-next-app/pages/about/index.js
@@ -0,0 +1,10 @@
+// ContactPage @ localhost:{port}/about
+const About = () => {
+  return (
+    <div className="container">
+      <p>this is the about page</p>
+    </div>
+  );
+};
+
+export default About;
