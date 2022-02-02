e2d570718036bfc97c201c6384cf31897a83992f
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-02-02 14:07:35 +0530

    TE5-T197 wrapped: Component with Layout and SnackbarProvider

--- react-courses-2/todo-fullstack/pages/_app.js
+++ react-courses-2/todo-fullstack/pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import { SnackbarProvider } from "notistack";
+import Layout from "../layout/Layout";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    // Wrapping Component with Snackbar provider and Layout
+    <Layout>
+      <SnackbarProvider maxSnack={2}>
+        <Component {...pageProps} />
+      </SnackbarProvider>
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
