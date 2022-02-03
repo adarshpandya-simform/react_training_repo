19bf5878906704330c5f37c76ca2ec9f429d4d4e
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-02-03 14:10:58 +0530

    TE5-T197 rendered: BasicCompoundPatternProblem and BasicCompoundPatternSolution component

--- react-courses-3/design-patterns-react/src/App.js
+++ react-courses-3/design-patterns-react/src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import BasicCompoundPatternProblem from "./components/BasicCompoundPatternProblem/BasicCompoundPatternProblem";
+import BasicCompoundPatternSolution from "./components/BasicCompoundPatternSolution/BasicCompoundPatternSolution";
 
 const App = () => {
-  return <div>hello world</div>;
+  return (
+    <div>
+      <span>Basic Compound Pattern Problem</span>
+      <BasicCompoundPatternProblem />
+      <br />
+      <span>Basic Compound Pattern Solution</span>
+      <BasicCompoundPatternSolution>
+        <BasicCompoundPatternSolution.On>
+          the switch is on
+        </BasicCompoundPatternSolution.On>
+        <BasicCompoundPatternSolution.Off>
+          the switch is off
+        </BasicCompoundPatternSolution.Off>
+        <BasicCompoundPatternSolution.Button />
+      </BasicCompoundPatternSolution>
+    </div>
+  );
 };
 
 export default App;
