65f05dc08433c826e4bd15ea9aec435493073e26
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-02-04 14:36:23 +0530

    TE5-T197 exported: AnimatedComponent4 from index

--- react-courses-3/react-animation-framer-motion/src/components/index.js
+++ react-courses-3/react-animation-framer-motion/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as AnimatedComponent1 } from "./AnimatedComponent1/AnimatedComponent1";
 export { default as AnimatedComponent2 } from "./AnimatedComponent2/AnimatedComponent2";
 export { default as AnimatedComponent3 } from "./AnimatedComponent3/AnimatedComponent3";
+export { default as AnimatedComponent4 } from "./AnimatedComponent4/AnimatedComponent4";
