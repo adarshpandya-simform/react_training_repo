d5275ead9e2c4c996bfb769cea41a98add3cc166
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-02-04 16:44:55 +0530

    TE5-T197 rendered: AnimatedModal with a toggle switch

--- react-courses-3/react-animation-framer-motion/src/App.js
+++ react-courses-3/react-animation-framer-motion/src/App.js
@@ -1,21 +1,35 @@
-import React from "react";
+import { AnimatePresence } from "framer-motion";
+import React, { useState } from "react";
 import {
 	AnimatedComponent1,
 	AnimatedComponent2,
 	AnimatedComponent3,
 	AnimatedComponent4,
 	AnimatedComponent5,
+	AnimatedModal,
 } from "./components";
 
 const App = () => {
+	const [showModal, setShowModal] = useState(false);
+
+	const modalHandler = () => {
+		setShowModal(true);
+	};
+
 	return (
-		<div className="container">
-			<AnimatedComponent1 />
-			<AnimatedComponent2 />
-			<AnimatedComponent3 />
-			<AnimatedComponent4 />
-			<AnimatedComponent5 />
-		</div>
+		<>
+			<div className="container">
+				<AnimatedComponent1 />
+				<AnimatedComponent2 />
+				<AnimatedComponent3 />
+				<AnimatedComponent4 />
+				<AnimatedComponent5 />
+				<button onClick={modalHandler}>open modal</button>
+			</div>
+			<AnimatePresence exitBeforeEnter>
+				{showModal && <AnimatedModal setShowModal={setShowModal} />}
+			</AnimatePresence>
+		</>
 	);
 };
 
