fb82cfdf9e1467e3650df2ae253dcd721d255192
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-02-04 14:35:54 +0530

    TE5-T197 created: AnimatedComponent4 to demonstrate usage of variants

---
+++ react-courses-3/react-animation-framer-motion/src/components/AnimatedComponent4/AnimatedComponent4.js
@@ -0,0 +1,44 @@
+import "./AnimatedComponent4.css";
+import { motion } from "framer-motion";
+import { useState } from "react";
+
+const outerSwitchVariants = {
+	on: {
+		background: "rgb(15, 247, 15)",
+	},
+	off: {
+		background: "rgb(189, 187, 187)",
+	},
+};
+
+const innerSwitchVariants = {
+	on: {
+		x: 30,
+	},
+	off: {
+		x: 0,
+	},
+};
+
+const AnimatedComponent4 = () => {
+	const [toggle, setToggle] = useState(false);
+	return (
+		<div>
+			<br />
+			<motion.div
+				variants={outerSwitchVariants}
+				animate={toggle ? "on" : "off"}
+				className="outer-switch"
+			>
+				<motion.div
+					onClick={() => setToggle((prevToggle) => !prevToggle)}
+					className="inner-switch"
+					variants={innerSwitchVariants}
+					animate={toggle ? "on" : "off"}
+				></motion.div>
+			</motion.div>
+		</div>
+	);
+};
+
+export default AnimatedComponent4;
