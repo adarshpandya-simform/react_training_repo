b4ccd908b3d840aea09e7b03cdc6f36dfb55ffcd
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-02-04 15:41:03 +0530

    TE5-T197 rendered: AnimatedComponent5 in App component

--- react-courses-3/react-animation-framer-motion/src/App.js
+++ react-courses-3/react-animation-framer-motion/src/App.js
@@ -1,20 +1,22 @@
 import React from "react";
 import {
-  AnimatedComponent1,
-  AnimatedComponent2,
-  AnimatedComponent3,
-  AnimatedComponent4,
+	AnimatedComponent1,
+	AnimatedComponent2,
+	AnimatedComponent3,
+	AnimatedComponent4,
+	AnimatedComponent5,
 } from "./components";
 
 const App = () => {
-  return (
-    <div className="container">
-      <AnimatedComponent1 />
-      <AnimatedComponent2 />
-      <AnimatedComponent3 />
-      <AnimatedComponent4 />
-    </div>
-  );
+	return (
+		<div className="container">
+			<AnimatedComponent1 />
+			<AnimatedComponent2 />
+			<AnimatedComponent3 />
+			<AnimatedComponent4 />
+			<AnimatedComponent5 />
+		</div>
+	);
 };
 
 export default App;
