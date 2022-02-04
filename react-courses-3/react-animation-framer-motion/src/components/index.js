97dc4e72cf80baee15660d33b9f09050f24fb4d9
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-02-04 16:42:55 +0530

    TE5-T197 exported: AnimatedModal from index

--- react-courses-3/react-animation-framer-motion/src/components/index.js
+++ react-courses-3/react-animation-framer-motion/src/components/index.js
@@ -3,3 +3,4 @@ export { default as AnimatedComponent2 } from "./AnimatedComponent2/AnimatedComp
 export { default as AnimatedComponent3 } from "./AnimatedComponent3/AnimatedComponent3";
 export { default as AnimatedComponent4 } from "./AnimatedComponent4/AnimatedComponent4";
 export { default as AnimatedComponent5 } from "./AnimatedComponent5/AnimatedComponent5";
+export { default as AnimatedModal } from "./AnimatedModal/AnimatedModal";
