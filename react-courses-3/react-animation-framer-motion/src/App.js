d9122ce713091b245c0fb85f7bd063d42b3a3d3d
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-02-04 13:54:56 +0530

    TE5-T197 rendered: AnimatedComponent3 in App component

--- react-courses-3/react-animation-framer-motion/src/App.js
+++ react-courses-3/react-animation-framer-motion/src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { AnimatedComponent1, AnimatedComponent2 } from "./components";
+import {
+  AnimatedComponent1,
+  AnimatedComponent2,
+  AnimatedComponent3,
+} from "./components";
 
 const App = () => {
   return (
     <div className="container">
       <AnimatedComponent1 />
       <AnimatedComponent2 />
+      <AnimatedComponent3 />
     </div>
   );
 };
