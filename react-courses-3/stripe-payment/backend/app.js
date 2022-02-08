39fee3f1dae28f1052e8273f109ef4b7014b439e
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-02-08 17:37:41 +0530

    TE5-T197 added: comments

--- react-courses-3/stripe-payment/backend/app.js
+++ react-courses-3/stripe-payment/backend/app.js
@@ -4,23 +4,30 @@ const stripe = require("stripe")(process.env.SECRET_KEY);
 const cors = require("cors");
 
 const PORT = process.env.PORT || 2000;
+// initializing express app
 const app = express();
+// using cors so request should not block
 app.use(cors());
 app.use(express.json());
 
+// defining store items
 const storeItems = new Map([
   [1, { priceInCents: 10000, name: "Learn React Today" }],
   [2, { priceInCents: 20000, name: "Learn CSS Today" }],
 ]);
 
+// post route @/checkout
 app.post("/checkout", async (req, res) => {
+  // getting session url from stripe
   try {
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       mode: "payment",
       line_items: req.body.items.map((item) => {
+        // finding item
         const storeItem = storeItems.get(item.id);
         return {
+          // defining options
           price_data: {
             currency: "inr",
             product_data: {
@@ -34,8 +41,10 @@ app.post("/checkout", async (req, res) => {
       success_url: `${"http://localhost:3000"}/success`,
       cancel_url: `${"http://localhost:3000"}/cancel`,
     });
+    // returning response
     res.json({ url: session.url, success: true });
   } catch (e) {
+    // catching errors
     res.status(500).json({ error: e.message });
   }
 });
--- react-courses-3/stripe-payment/client/src/App.js
+++ react-courses-3/stripe-payment/client/src/App.js
@@ -1,11 +1,15 @@
 import axios from "axios";
 import { useState } from "react";
 
+// App component
 const App = () => {
+  // maintaining state for react and css course
   const [reactCourse, setReactCourse] = useState(0);
   const [cssCourse, setCssCourse] = useState(0);
+  // maintaining state for error
   const [error, setError] = useState(false);
 
+  // helper function to make post request to server with item ids in body
   const paymentAcceptHandler = async () => {
     await axios
       .post(
@@ -19,6 +23,7 @@ const App = () => {
         { headers: { "Content-Type": "application/json" } }
       )
       .then((res) => {
+        // handling response
         if (res.data) {
           if (res.data.success) {
             window.location = res.data.url;
@@ -35,22 +40,26 @@ const App = () => {
       });
   };
 
+  // helper fn to increse amount of react course
   const handleReactCourseIncrement = () => {
     setReactCourse((prevCourse) => prevCourse + 1);
   };
 
+  // helper fn to decrese amount of react course
   const handleReactCourseDecrement = () => {
     if (reactCourse !== 0) {
       setReactCourse((prevCourse) => prevCourse - 1);
     }
   };
 
+  // helper fn to increse amount of css course
   const handleCssCourseDecrement = () => {
     if (cssCourse !== 0) {
       setCssCourse((prevCourse) => prevCourse - 1);
     }
   };
 
+  // helper fn to decrese amount of css course
   const handleCssCourseIncrement = () => {
     setCssCourse((prevCourse) => prevCourse + 1);
   };
@@ -74,6 +83,7 @@ const App = () => {
       <button onClick={paymentAcceptHandler}>pay using stripe</button>
       <br />
       <br />
+      {/* conditionally rendering error */}
       {error && (
         <p>unexpected error occured while paying. try again later...</p>
       )}
