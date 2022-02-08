f793d6e4043645ae9656cdd5f8169e05d53f9142
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-02-08 12:35:30 +0530

    TE5-T197 rendered: MenuButton,Drawer,Backdrop component

--- react-courses-3/animated-drawer/src/App.js
+++ react-courses-3/animated-drawer/src/App.js
@@ -1,7 +1,26 @@
-import React from "react";
+import { useState } from "react";
+import { AnimatePresence } from "framer-motion";
+import "./App.css";
+import { Backdrop, Drawer, MenuButton } from "./components";
 
 const App = () => {
-  return <div>hello</div>;
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <div className="app-container">
+        {/* menu button */}
+        <MenuButton isOpen={isOpen} setIsOpen={setIsOpen} />
+      </div>
+      {/* backdrop */}
+      <AnimatePresence exitBeforeEnter>
+        {isOpen && <Backdrop />}
+      </AnimatePresence>
+      {/* drawer */}
+      <AnimatePresence exitBeforeEnter>
+        {isOpen && <Drawer setIsOpen={setIsOpen} />}
+      </AnimatePresence>
+    </>
+  );
 };
 
 export default App;
