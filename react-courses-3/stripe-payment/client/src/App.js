cd979f23024f448cc0df95ac204922c908f60d64
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-02-08 15:21:24 +0530

    TE5-T197 added: functionality to dynamically add course amounts

--- react-courses-3/stripe-payment/client/src/App.js
+++ react-courses-3/stripe-payment/client/src/App.js
@@ -1,14 +1,19 @@
 import axios from "axios";
+import { useState } from "react";
 
 const App = () => {
+  const [reactCourse, setReactCourse] = useState(0);
+  const [cssCourse, setCssCourse] = useState(0);
+  const [error, setError] = useState(false);
+
   const paymentAcceptHandler = async () => {
     await axios
       .post(
         "http://localhost:2000/checkout",
         {
           items: [
-            { id: 1, quantity: 2 },
-            { id: 2, quantity: 3 },
+            { id: 1, quantity: reactCourse },
+            { id: 2, quantity: cssCourse },
           ],
         },
         { headers: { "Content-Type": "application/json" } }
@@ -17,6 +22,7 @@ const App = () => {
         if (res.data) {
           if (res.data.success) {
             window.location = res.data.url;
+            setError(false);
           } else {
             console.log("error", res.data);
           }
@@ -25,17 +31,52 @@ const App = () => {
         }
       })
       .catch((error) => {
-        console.log(String(error));
+        setError(true);
       });
+  };
+
+  const handleReactCourseIncrement = () => {
+    setReactCourse((prevCourse) => prevCourse + 1);
+  };
+
+  const handleReactCourseDecrement = () => {
+    if (reactCourse !== 0) {
+      setReactCourse((prevCourse) => prevCourse - 1);
+    }
+  };
+
+  const handleCssCourseDecrement = () => {
+    if (cssCourse !== 0) {
+      setCssCourse((prevCourse) => prevCourse - 1);
+    }
+  };
+
+  const handleCssCourseIncrement = () => {
+    setCssCourse((prevCourse) => prevCourse + 1);
   };
 
   return (
     <div>
-      <button onClick={paymentAcceptHandler}>pay using stripe</button>
       <code>
         the payment gateway is still in test mode. test it with 4242 4242 4242
         4242 card
       </code>
+      <br />
+      <button onClick={handleReactCourseIncrement}>+</button>
+      <input type={"number"} value={reactCourse} readOnly />
+      <button onClick={handleReactCourseDecrement}>-</button>
+      <br />
+      <button onClick={handleCssCourseIncrement}>+</button>
+      <input type={"number"} value={cssCourse} readOnly />
+      <button onClick={handleCssCourseDecrement}>-</button>
+      <br />
+      <br />
+      <button onClick={paymentAcceptHandler}>pay using stripe</button>
+      <br />
+      <br />
+      {error && (
+        <p>unexpected error occured while paying. try again later...</p>
+      )}
     </div>
   );
 };
