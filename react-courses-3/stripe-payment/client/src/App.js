af2b019f441c4cc65cb46519b7d2ea5626e0dd8f
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-02-08 15:03:31 +0530

    TE5-T197 created: App component to send checkout request with items

--- react-courses-3/stripe-payment/client/src/App.js
+++ react-courses-3/stripe-payment/client/src/App.js
@@ -1,7 +1,43 @@
-import React from "react";
+import axios from "axios";
 
 const App = () => {
-  return <div>hello world</div>;
+  const paymentAcceptHandler = async () => {
+    await axios
+      .post(
+        "http://localhost:2000/checkout",
+        {
+          items: [
+            { id: 1, quantity: 2 },
+            { id: 2, quantity: 3 },
+          ],
+        },
+        { headers: { "Content-Type": "application/json" } }
+      )
+      .then((res) => {
+        if (res.data) {
+          if (res.data.success) {
+            window.location = res.data.url;
+          } else {
+            console.log("error", res.data);
+          }
+        } else {
+          console.log("not successful");
+        }
+      })
+      .catch((error) => {
+        console.log(String(error));
+      });
+  };
+
+  return (
+    <div>
+      <button onClick={paymentAcceptHandler}>pay using stripe</button>
+      <code>
+        the payment gateway is still in test mode. test it with 4242 4242 4242
+        4242 card
+      </code>
+    </div>
+  );
 };
 
 export default App;
