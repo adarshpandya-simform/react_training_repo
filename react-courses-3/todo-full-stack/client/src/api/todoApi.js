b3880db5c572ceb7fa14ef29f5a44719ce03d0b8
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-02-09 17:08:10 +0530

    TE5-T197 added: comments in api

--- react-courses-3/todo-full-stack/client/src/api/todoApi.js
+++ react-courses-3/todo-full-stack/client/src/api/todoApi.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+// check mode with process.env.NODE_ENV
 const mode = process.env.NODE_ENV;
 
+// creating axios instance
 const instance = axios.create({
   baseURL:
     mode === "development"
@@ -10,16 +12,19 @@ const instance = axios.create({
   headers: { "Content-Type": "application/json" },
 });
 
+// request to get all todos
 export const getTodosRequest = async () => {
   const { data } = await instance.get("/todos");
   return data;
 };
 
+// request to delete todo with given id
 export const deleteTodoRequest = async (id) => {
   const { data } = await instance.delete(`/delete-todo/${id}`);
   return data;
 };
 
+// request to update todo with given id with given data
 export const updateTodoRequest = async ({ id, title, completed }) => {
   const { data } = await instance.patch("/update-todo", {
     id,
@@ -29,6 +34,7 @@ export const updateTodoRequest = async ({ id, title, completed }) => {
   return data;
 };
 
+// request to add todo
 export const addTodoRequest = async ({ title }) => {
   const { data } = await instance.post("/add-todo", { title });
   return data;
