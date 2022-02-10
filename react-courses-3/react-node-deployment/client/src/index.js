7ebe0ea09bab3de21026cec20df221459519f451
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-02-10 14:04:45 +0530

    TE5-T197 wrapped: App inside BrowserRouter, ChakraProvider(with theme as prop) and ColorModeScript

--- react-courses-3/react-node-deployment/client/src/index.js
+++ react-courses-3/react-node-deployment/client/src/index.js
@@ -2,12 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import "./index.css";
+import { theme } from "./theme/theme";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <App />
+      </ChakraProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
