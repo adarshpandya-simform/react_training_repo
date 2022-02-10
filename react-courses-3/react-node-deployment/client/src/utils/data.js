e81d030190b3ebdd57fd2dff1733cc89d1055ac2
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-02-10 15:36:27 +0530

    TE5-T197 extracted links data in data.js

---
+++ react-courses-3/react-node-deployment/client/src/utils/data.js
@@ -0,0 +1,63 @@
+import {
+  AspectRatioComponent,
+  BoxComponent,
+  CenterComponent,
+  CustomizedComponent,
+  CustomStylesComponent,
+  DarkModeButton,
+  DarkModeComponent,
+  ForcedModeComponents,
+  IconsComponent,
+  MenuComponent,
+  TabsComponent,
+} from "../components";
+
+export const links = [
+  { path: "/", name: "Box", component: <BoxComponent /> },
+  { path: "/dark-mode", name: "Dark Mode", component: <DarkModeButton /> },
+  {
+    path: "/dark-mode-component",
+    name: "Component Reacting to dark mode",
+    component: <DarkModeComponent />,
+  },
+  {
+    path: "/forced-mode-components",
+    name: "Component With forced modes",
+    component: <ForcedModeComponents />,
+  },
+  {
+    path: "/custom-styled-components",
+    name: "Custom Styled components",
+    component: <CustomStylesComponent />,
+  },
+  {
+    path: "/customized-component",
+    name: "Custom Style Override Component",
+    component: <CustomizedComponent />,
+  },
+  {
+    path: "/tabs-component",
+    name: "Tabs Layout",
+    component: <TabsComponent />,
+  },
+  {
+    path: "/menu-component",
+    name: "Menu with items",
+    component: <MenuComponent />,
+  },
+  {
+    path: "/aspect-ration",
+    name: "Aspect Ratio",
+    component: <AspectRatioComponent />,
+  },
+  {
+    path: "/icon-buttons",
+    name: "Icon Buttons",
+    component: <IconsComponent />,
+  },
+  {
+    path: "/center-component",
+    name: "Center Component",
+    component: <CenterComponent />,
+  },
+];
