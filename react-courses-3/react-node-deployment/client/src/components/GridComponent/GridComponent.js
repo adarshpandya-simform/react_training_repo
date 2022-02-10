24b9c02e415ef447725c579ea66ff2d281091460
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-02-10 16:20:26 +0530

    TE5-T197 created: GridComponent for demonstrating chakra-ui Grid

---
+++ react-courses-3/react-node-deployment/client/src/components/GridComponent/GridComponent.js
@@ -0,0 +1,40 @@
+import { Center, Grid, GridItem, Text } from "@chakra-ui/react";
+
+const GridComponent = () => {
+  return (
+    <Grid
+      h="500px"
+      templateRows="repeat(3, 1fr)"
+      templateColumns="repeat(5, 1fr)"
+      gap={4}
+    >
+      <GridItem rowSpan={2} colSpan={1} bg="bisque">
+        <Center h="full">
+          <Text>Menu</Text>
+        </Center>
+      </GridItem>
+      <GridItem colSpan={4} bg="lightcoral">
+        <Center h="full">
+          <Text>Header</Text>
+        </Center>
+      </GridItem>
+      <GridItem colSpan={3} bg="lightgreen">
+        <Center h="full">
+          <Text>Content</Text>
+        </Center>
+      </GridItem>
+      <GridItem colSpan={1} bg="lightblue">
+        <Center h="full">
+          <Text>Ads</Text>
+        </Center>
+      </GridItem>
+      <GridItem colSpan={5} bg="lightgray">
+        <Center h="full">
+          <Text>Footer</Text>
+        </Center>
+      </GridItem>
+    </Grid>
+  );
+};
+
+export default GridComponent;
