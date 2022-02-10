689c2d6fd20648412989c39fd773b96d5a5c7785
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-02-10 14:10:43 +0530

    TE5-T197 exported: all chakra-ui based components through index file

---
+++ react-courses-3/react-node-deployment/client/src/components/index.js
@@ -0,0 +1,6 @@
+export { default as BoxComponent } from "./Box/BoxComponent";
+export { default as DarkModeButton } from "./DarkMode/DarkModeButton";
+export { default as DarkModeComponent } from "./DarkModeComponent/DarkModeComponent";
+export { default as ForcedModeComponents } from "./ForcedModeComponents/ForcedModeComponents";
+export { default as CustomStylesComponent } from "./CustomStylesComponent/CustomStylesComponent";
+export { default as CustomizedComponent } from "./CustomizedComponent/CustomizedComponent";
