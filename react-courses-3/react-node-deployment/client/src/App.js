b94fb84df3cb86808b6e57fa14ddf425037789f9
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-02-10 14:11:50 +0530

    TE5-T197 rendered: all chakra-ui components on their respective routes

--- react-courses-3/react-node-deployment/client/src/App.js
+++ react-courses-3/react-node-deployment/client/src/App.js
@@ -1,16 +1,45 @@
 import { Link, Route, Routes } from "react-router-dom";
+import {
+  BoxComponent,
+  CustomizedComponent,
+  CustomStylesComponent,
+  DarkModeButton,
+  DarkModeComponent,
+  ForcedModeComponents,
+} from "./components";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Link to={"/"}>home</Link>
-      <Link to={"/about"}>about</Link>
+    <div>
+      <br />
+      <div className="links">
+        <Link to={"/"}>Box</Link>
+        <Link to={"/dark-mode"}>Dark Mode</Link>
+        <Link to={"/dark-mode-component"}>Component Reacting to dark mode</Link>
+        <Link to={"/forced-mode-components"}>Component With forced modes</Link>
+        <Link to={"/custom-styled-components"}>Custom Styled components</Link>
+        <Link to={"/customized-component"}>
+          Custom Style Override Component
+        </Link>
+      </div>
+      <br />
       <Routes>
-        <Route path="/" element={<p>home</p>} />
-        <Route path="/about" element={<p>about</p>} />
+        <Route path="/" element={<BoxComponent />} />
+        <Route path="/dark-mode" element={<DarkModeButton />} />
+        <Route path="/dark-mode-component" element={<DarkModeComponent />} />
+        <Route
+          path="/forced-mode-components"
+          element={<ForcedModeComponents />}
+        />
+        <Route
+          path="/custom-styled-components"
+          element={<CustomStylesComponent />}
+        />
+        <Route path="/customized-component" element={<CustomizedComponent />} />
       </Routes>
     </div>
   );
-}
+};
 
 export default App;
