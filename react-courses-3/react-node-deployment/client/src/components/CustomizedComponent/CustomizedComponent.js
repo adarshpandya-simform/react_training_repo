1e351b4e3c42e9454de1cb42f5a89970da71af9a
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-02-10 14:09:04 +0530

    TE5-T197 created: CustomizedComponent which overrides default styles of button and introduces a new variant

---
+++ react-courses-3/react-node-deployment/client/src/components/CustomizedComponent/CustomizedComponent.js
@@ -0,0 +1,7 @@
+import { Button } from "@chakra-ui/react";
+
+const CustomizedComponent = () => {
+  return <Button variant={"dank"}>customly styled button</Button>;
+};
+
+export default CustomizedComponent;
