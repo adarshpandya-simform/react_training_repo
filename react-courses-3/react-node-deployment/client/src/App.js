081ff9ba8bc7667349474a630a8744557cf229bd
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-02-10 15:35:11 +0530

    TE5-T197 moved: links data in separate utils

--- react-courses-3/react-node-deployment/client/src/App.js
+++ react-courses-3/react-node-deployment/client/src/App.js
@@ -1,48 +1,20 @@
 import { Link, Route, Routes } from "react-router-dom";
-import {
-  BoxComponent,
-  CustomizedComponent,
-  CustomStylesComponent,
-  DarkModeButton,
-  DarkModeComponent,
-  ForcedModeComponents,
-} from "./components";
-import { Button } from "@chakra-ui/react";
+import { Button, Divider } from "@chakra-ui/react";
+import { Fragment } from "react";
+import { links } from "./utils/data";
 import "./App.css";
-
-const links = [
-  { path: "/", name: "Box", component: <BoxComponent /> },
-  { path: "/dark-mode", name: "Dark Mode", component: <DarkModeButton /> },
-  {
-    path: "/dark-mode-component",
-    name: "Component Reacting to dark mode",
-    component: <DarkModeComponent />,
-  },
-  {
-    path: "/forced-mode-components",
-    name: "Component With forced modes",
-    component: <ForcedModeComponents />,
-  },
-  {
-    path: "/custom-styled-components",
-    name: "Custom Styled components",
-    component: <CustomStylesComponent />,
-  },
-  {
-    path: "/customized-component",
-    name: "Custom Style Override Component",
-    component: <CustomizedComponent />,
-  },
-];
 
 const App = () => {
   return (
     <div className="app-container">
       <div className="links">
         {links.map((link) => (
-          <Link key={link.path} to={link.path}>
-            <Button variant={"link"}>{link.name}</Button>
-          </Link>
+          <Fragment key={link.path}>
+            <Link to={link.path}>
+              <Button variant={"link"}>{link.name}</Button>
+            </Link>
+            <Divider />
+          </Fragment>
         ))}
       </div>
       <div className="components">
