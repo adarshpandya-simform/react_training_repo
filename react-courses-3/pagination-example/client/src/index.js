0feacc7b7b789b89f766271202e84ea127e91016
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-02-14 14:25:56 +0530

    TE5-T197 wrapped: App component in BrowserRouter

--- react-courses-3/pagination-example/client/src/index.js
+++ react-courses-3/pagination-example/client/src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 
+// Wrapping App component with BrowserRouter
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+	<React.StrictMode>
+		<Router>
+			<App />
+		</Router>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
