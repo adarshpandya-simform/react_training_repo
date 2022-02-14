1c7309f17ba41a75b9155f05fe2b72cd6c67752f
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-02-14 17:09:17 +0530

    TE5-T197 rendered: InputTodo and DisplayTodo component in App component, fetched: todos on render

--- react-courses-3/react-supabase/src/App.js
+++ react-courses-3/react-supabase/src/App.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
+import DisplayTodo from "./components/DisplayTodo";
+import InputTodo from "./components/InputTodo";
+import { useTodos } from "./hooks/useTodos";
+import { getTodos } from "./db/db.operations";
 
+// App component
 const App = () => {
-  return <div>App</div>;
+  const { fetchTodosDispatcher, setLoadingDispatcher } = useTodos();
+
+  // fetching todos on app mount
+  useEffect(() => {
+    (async () => {
+      setLoadingDispatcher(true);
+      const { data, error } = await getTodos();
+      if (error) {
+        console.log("error occured");
+        console.log(error);
+      } else {
+        fetchTodosDispatcher(data);
+      }
+      setLoadingDispatcher(false);
+    })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <>
+      {/* Rendering InputTodo and DisplayTodo*/}
+      <InputTodo />
+      <DisplayTodo />
+    </>
+  );
 };
 
 export default App;
