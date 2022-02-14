e25bad29e49d6f0574c33fe9e48e9fc756533433
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-02-14 17:02:17 +0530

    TE5-T917 wrapped: App inside TodoProvider

--- react-courses-3/react-supabase/src/index.js
+++ react-courses-3/react-supabase/src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { TodoProvider } from "./contexts/TodoContext";
 import "./index.css";
 
+// wrapped: App component inside TodoProvider
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TodoProvider>
+      <App />
+    </TodoProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
