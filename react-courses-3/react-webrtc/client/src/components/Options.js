fa9b46e280bbe91797a1b18801c8c7d8dde86b27
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-02-15 17:18:29 +0530

    TE5-T197 created: Options component

---
+++ react-courses-3/react-webrtc/client/src/components/Options.js
@@ -0,0 +1,93 @@
+import {
+  Button,
+  TextField,
+  Grid,
+  Typography,
+  Container,
+  Paper,
+} from "@material-ui/core";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { Assignment, Phone, PhoneDisabled } from "@material-ui/icons";
+import { useContext, useState } from "react";
+import { SocketContext } from "../context/SocketContext";
+import { getOptionsStyle } from "../theme/theme";
+
+// Options component for rendering name, call id and call buttons plus notifications
+const Options = ({ children }) => {
+  // extracting state and helpers from context
+  const { me, callAccepted, name, setName, leaveCall, callUser, callEnded } =
+    useContext(SocketContext);
+  const [idToCall, setIdToCall] = useState("");
+  // accessing styles
+  const classes = getOptionsStyle();
+
+  return (
+    <Container className={classes.container}>
+      <Paper elevation={10} className={classes.paper}>
+        <form className={classes.root} noValidate autoComplete="off">
+          <Grid container className={classes.gridContainer}>
+            <Grid item xs={12} md={6} className={classes.padding}>
+              <Typography gutterBottom variant="h6">
+                Account Info
+              </Typography>
+              <TextField
+                label="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                fullWidth
+              />
+              <CopyToClipboard text={me} className={classes.margin}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                  startIcon={<Assignment fontSize="large" />}
+                >
+                  Copy Your ID
+                </Button>
+              </CopyToClipboard>
+            </Grid>
+            <Grid item xs={12} md={6} className={classes.padding}>
+              <Typography gutterBottom variant="h6">
+                Make a call
+              </Typography>
+              <TextField
+                label="ID to call"
+                value={idToCall}
+                onChange={(e) => setIdToCall(e.target.value)}
+                fullWidth
+              />
+              {callAccepted && !callEnded ? (
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  startIcon={<PhoneDisabled fontSize="large" />}
+                  fullWidth
+                  onClick={leaveCall}
+                  className={classes.margin}
+                >
+                  Hang Up
+                </Button>
+              ) : (
+                <Button
+                  variant="contained"
+                  color="primary"
+                  startIcon={<Phone fontSize="large" />}
+                  fullWidth
+                  onClick={() => callUser(idToCall)}
+                  className={classes.margin}
+                >
+                  Call
+                </Button>
+              )}
+            </Grid>
+          </Grid>
+        </form>
+      </Paper>
+      {/* rendering call notification if exists */}
+      {children}
+    </Container>
+  );
+};
+
+export default Options;
