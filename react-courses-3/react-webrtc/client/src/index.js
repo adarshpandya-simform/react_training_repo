d7251a2dda960d27991120d9a98c01dccc6875aa
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-02-15 17:15:17 +0530

    TE5-T197 wrapped: App component inside SocketProvider component

--- react-courses-3/react-webrtc/client/src/index.js
+++ react-courses-3/react-webrtc/client/src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { SocketProvider } from "./context/SocketContext";
 import "./index.css";
 
+// Wrapping App inside SocketProvvider
+// global context
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SocketProvider>
+      <App />
+    </SocketProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
