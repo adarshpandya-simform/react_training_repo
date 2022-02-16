8b864a831302ea770f08e03e41aeb72eaffaa44a
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-02-16 13:05:48 +0530

    TE5-T197 wrapped: App inside TodoProvider

--- react-practice/mui-todo/src/index.js
+++ react-practice/mui-todo/src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { TodoProvider } from "./context/TodoContext";
 import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TodoProvider>
+      <App />
+    </TodoProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
