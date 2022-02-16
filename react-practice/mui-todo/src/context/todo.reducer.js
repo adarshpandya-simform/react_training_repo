91c29101a2082d24b6ee4124da88bc64fab82ef8
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-02-16 12:59:16 +0530

    TE5-T197 created: reducer for  TodoContext

---
+++ react-practice/mui-todo/src/context/todo.reducer.js
@@ -0,0 +1,34 @@
+import { ADD_TODO, CHECK_TODO, DELETE_TODO } from "./todo.actions";
+import { nanoid } from "nanoid";
+
+export const todoReducer = (state, { type, payload }) => {
+  switch (type) {
+    case ADD_TODO:
+      return {
+        ...state,
+        todos: [
+          ...state.todos,
+          { id: nanoid(), title: payload.title, completed: false },
+        ],
+      };
+    case DELETE_TODO:
+      return {
+        ...state,
+        todos: [...state.todos.filter((todo) => todo.id !== payload.id)],
+      };
+    case CHECK_TODO:
+      return {
+        ...state,
+        todos: [
+          ...state.todos.map((todo) => {
+            if (todo.id === payload.id) {
+              todo.completed = payload.completed;
+            }
+            return todo;
+          }),
+        ],
+      };
+    default:
+      return state;
+  }
+};
