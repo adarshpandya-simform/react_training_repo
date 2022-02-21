f549fa18ff1e977e6da8ef2a7bf93a9198b19e22
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-02-21 16:08:14 +0530

    TE5-T197 created: Dropdown component with tailwind utility classes

---
+++ react-practice/tailwind-css/src/components/Dropdown/Dropdown.js
@@ -0,0 +1,55 @@
+import { Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/solid";
+import { classNames } from "../../utils/getClassNames";
+
+const designations = [
+	{ id: 1, name: "Junior Developer" },
+	{ id: 2, name: "Senior Developer" },
+	{ id: 3, name: "Team Manager" },
+	{ id: 4, name: "CTO" },
+];
+
+const Dropdown = () => {
+	return (
+		<Menu as="div" className="relative inline-block text-left">
+			<div>
+				<Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+					Select Designation
+					<ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+				</Menu.Button>
+			</div>
+
+			<Transition
+				as={Fragment}
+				enter="transition ease-out duration-100"
+				enterFrom="transform opacity-0 scale-95"
+				enterTo="transform opacity-100 scale-100"
+				leave="transition ease-in duration-75"
+				leaveFrom="transform opacity-100 scale-100"
+				leaveTo="transform opacity-0 scale-95"
+			>
+				<Menu.Items className="origin-bottom-left absolute mt-2 w-56 rounded-md shadow-lg bg-slate-800 ring-1 ring-black ring-opacity-5 focus:outline-none">
+					<div className="py-1">
+						{designations.map((designation) => (
+							<Menu.Item key={designation.id}>
+								{({ active }) => (
+									<p
+										className={classNames(
+											active ? "bg-slate-900 text-white" : "text-gray-500",
+											"block px-4 py-2 text-sm transition-all"
+										)}
+									>
+										{designation.name}
+									</p>
+								)}
+							</Menu.Item>
+						))}
+					</div>
+				</Menu.Items>
+			</Transition>
+		</Menu>
+	);
+};
+
+export default Dropdown;
