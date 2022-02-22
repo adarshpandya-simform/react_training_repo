71ec34e27307d060947bcf782778b130e3e945ba
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-02-22 17:15:20 +0530

    TE5-T197 wrapped: Component with context providers

--- react-practice/next-app/pages/_app.tsx
+++ react-practice/next-app/pages/_app.tsx
@@ -1,8 +1,16 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import "../styles/globals.css";
+import { DarkThemeProvider } from "../context/DarkModeContext";
+import { SnackbarProvider } from "notistack";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
+  return (
+    <DarkThemeProvider>
+      <SnackbarProvider maxSnack={4}>
+        <Component {...pageProps} />
+      </SnackbarProvider>
+    </DarkThemeProvider>
+  );
+};
 
 export default MyApp;
