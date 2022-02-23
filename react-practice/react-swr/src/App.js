7b67ca82d8a0f07a12dc434665b35dd0a06700fc
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-02-23 14:50:37 +0530

    TE5-T197 added: Routings in App component

--- react-practice/react-swr/src/App.js
+++ react-practice/react-swr/src/App.js
@@ -1,5 +1,23 @@
+import { Link, Route, Routes } from "react-router-dom";
+import AddTodoPage from "./pages/AddTodoPage";
+import TodosPage from "./pages/TodosPage";
+import "./App.css";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <header>
+        <Link to="/">Home</Link>
+        <Link to="/todos">Todos</Link>
+        <Link to="/add-todo">Add Todo</Link>
+      </header>
+      <Routes>
+        <Route path="/" element={<p>home route</p>} />
+        <Route path="/todos" element={<TodosPage />} />
+        <Route path="/add-todo" element={<AddTodoPage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
