f872360fc2de215d01b3f5978c20b3e7290bcc48
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-02-23 14:46:04 +0530

    TE5-T197 created: api helpers

---
+++ react-practice/react-swr/src/api/api.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:2000/api";
+axios.defaults.headers.common["Content-Type"] = "application/json";
+
+export const fetchTodos = async () => {
+  const { data } = await axios("/todos");
+  return data;
+};
+
+export const addTodo = async (title) => {
+  const { data } = await axios.post("/add-todo", { title });
+  return data;
+};
+
+export const deleteTodo = async (id) => {
+  const { data } = await axios.delete(`/delete-todo/${id}`);
+  return data;
+};
