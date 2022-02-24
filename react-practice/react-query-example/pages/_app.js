7b717fc47fe0ffb4dc120de59cc63f1086bd31c3
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-02-24 16:57:54 +0530

    TE5-T197 wrapped: main component with QueryClientProvider and Layout

--- react-practice/react-query-example/pages/_app.js
+++ react-practice/react-query-example/pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Layout } from "../src/layout";
+import "../styles/globals.css";
 
+// initializing and exporting queryClient
+export const queryClient = new QueryClient();
+
+// Wrapping main Component with Layout and QueryClientProvider
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
