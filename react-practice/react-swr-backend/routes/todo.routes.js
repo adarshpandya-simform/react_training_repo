e7491c0beb6245f4d5688608740fdb5538d73902
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-02-24 16:47:44 +0530

    TE5-T197 configured: routes with db

--- react-practice/react-swr-backend/routes/todo.routes.js
+++ react-practice/react-swr-backend/routes/todo.routes.js
@@ -1,46 +1,45 @@
 import { Router } from "express";
-import { nanoid } from "nanoid";
+import mongoose from "mongoose";
+import { TodoModel } from "../models/todo.model.js";
 
 // exporting router
 export const router = Router();
 
-// initial dummy data
-let initialTodos = [
-  {
-    id: "1",
-    title: "Title 1",
-  },
-  {
-    id: "2",
-    title: "Title 2",
-  },
-  {
-    id: "3",
-    title: "Title 3",
-  },
-  {
-    id: "4",
-    title: "Title 4",
-  },
-];
-
 // handlers
 
 // @get /todos
-router.get("/todos/", (req, res) => {
-  res.json({ todos: initialTodos, success: true });
+router.get("/todos/", async (req, res) => {
+  const todos = await TodoModel.find({});
+  res.json({ todos, success: true });
+});
+
+// @get /todo/:id
+router.get("/todo/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const todo = await TodoModel.findById(id);
+    res.json({ todo, success: true });
+  } catch (error) {
+    res.json({ error, success: false });
+  }
 });
 
 // @post /add-todo
-router.post("/add-todo", (req, res) => {
-  const todo = { id: nanoid(), title: req.body.title };
-  initialTodos.push(todo);
+router.post("/add-todo", async (req, res) => {
+  const title = req.body.title;
+  const description = req.body.description;
+  const todoCreator = new TodoModel({ title, description });
+  const todo = await todoCreator.save();
   res.json({ todo, success: true });
 });
 
 // @delete /delete-todo/:id
-router.delete("/delete-todo/:id", (req, res) => {
+router.delete("/delete-todo/:id", async (req, res) => {
   const { id } = req.params;
-  initialTodos = initialTodos.filter((todo) => todo.id !== id);
-  res.json({ id, success: true });
+  try {
+    const todo = await TodoModel.findByIdAndDelete(id);
+    res.json({ todo, success: true });
+  } catch (error) {
+    res.json({ error, success: false });
+  }
 });
