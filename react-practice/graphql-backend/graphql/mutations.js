9e82465f4f174bd9d03b66742d239e8a7f3b4a37
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-02-28 16:50:39 +0530

    TE5-T197 updated: mutations with db helpers

--- react-practice/graphql-backend/graphql/mutations.js
+++ react-practice/graphql-backend/graphql/mutations.js
@@ -1,66 +1,44 @@
-import { authorsArr, booksArr } from "../data/data.js";
 import { nanoid } from "nanoid";
+import {
+  deleteAuthor,
+  insertAuthor,
+  updateAuthor,
+} from "../helpers/author.helper.js";
+import { deleteBook, insertBook, updateBook } from "../helpers/book.helper.js";
 
 // mutations for gql
 export const mutations = {
-  createBook: (_, { id, title }) => {
+  createBook: async (_, { id, title }) => {
     const book = {
       id,
       title,
       bookId: nanoid(),
     };
-    booksArr.push(book);
-    return book;
+    const insertedBook = await insertBook(book);
+    return insertedBook;
   },
-  createAuthor: (_, { name }) => {
+  createAuthor: async (_, { name }) => {
     const author = {
       authorId: nanoid(),
       name,
     };
-    authorsArr.push(author);
-    return author;
+    const insertedAuthor = await insertAuthor(author);
+    return insertedAuthor;
   },
-  updateBook: (_, { id, title, bookId }) => {
-    const book = booksArr.filter((book) => book.bookId === bookId);
-    if (book.length !== 0) {
-      booksArr = booksArr.map((book) => {
-        if (book.bookId === bookId) {
-          book.title = title;
-          book.id = id;
-        }
-        return book;
-      });
-      return book;
-    }
-    return null;
+  updateBook: async (_, { id, title, bookId }) => {
+    const updatedBook = await updateBook(bookId, id, title);
+    return updatedBook;
   },
   updateAuthor: (_, { authorId, name }) => {
-    const author = authorsArr.filter((author) => author.authorId === authorId);
-    if (author.length !== 0) {
-      authorsArr = authorsArr.map((author) => {
-        if (author.authorId === authorId) {
-          author.name = name;
-        }
-        return author;
-      });
-      return author;
-    }
-    return null;
+    const updatedAuthor = updateAuthor(authorId, name);
+    return updatedAuthor;
   },
-  deleteBook: (_, { bookId }) => {
-    const book = booksArr.find((book) => book.bookId === bookId);
-    if (book.length !== 0) {
-      booksArr = booksArr.filter((book) => book.bookId !== bookId);
-      return book[0];
-    }
-    return null;
+  deleteBook: async (_, { bookId }) => {
+    const deletedBook = await deleteBook(bookId);
+    return deletedBook;
   },
-  deleteAuthor: (_, { authorId }) => {
-    const author = authorsArr.find((author) => author.authorId === authorId);
-    if (author.length !== 0) {
-      authorsArr = authorsArr.filter((author) => author.authorId !== authorId);
-      return author[0];
-    }
-    return null;
+  deleteAuthor: async (_, { authorId }) => {
+    const deletedAuthor = await deleteAuthor(authorId);
+    return deletedAuthor;
   },
 };
