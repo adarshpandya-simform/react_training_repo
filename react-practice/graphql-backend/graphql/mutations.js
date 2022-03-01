fbef74247d73be4c84a755c8365170f24321e88b
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-03-01 11:39:20 +0530

    TE5-T197 added: pubsub publish event in every mutation

--- react-practice/graphql-backend/graphql/mutations.js
+++ react-practice/graphql-backend/graphql/mutations.js
@@ -5,6 +5,15 @@ import {
   updateAuthor,
 } from "../helpers/author.helper.js";
 import { deleteBook, insertBook, updateBook } from "../helpers/book.helper.js";
+import { pubsub } from "./pubsubHelper.js";
+import {
+  BOOK_CREATED,
+  AUTHOR_CREATED,
+  BOOK_UPDATED,
+  BOOK_DELETED,
+  AUTHOR_UPDATED,
+  AUTHOR_DELETED,
+} from "./triggers.js";
 
 // mutations for gql
 export const mutations = {
@@ -15,30 +24,65 @@ export const mutations = {
       bookId: nanoid(),
     };
     const insertedBook = await insertBook(book);
+
+    pubsub.publish(BOOK_CREATED, {
+      bookCreated: book,
+    });
+
     return insertedBook;
   },
+
   createAuthor: async (_, { name }) => {
     const author = {
       authorId: nanoid(),
       name,
     };
     const insertedAuthor = await insertAuthor(author);
+
+    pubsub.publish(AUTHOR_CREATED, {
+      authorCreated: author,
+    });
+
     return insertedAuthor;
   },
+
   updateBook: async (_, { id, title, bookId }) => {
     const updatedBook = await updateBook(bookId, id, title);
+
+    pubsub.publish(BOOK_UPDATED, {
+      bookUpdated: updatedBook,
+    });
+
     return updatedBook;
   },
+
   updateAuthor: (_, { authorId, name }) => {
     const updatedAuthor = updateAuthor(authorId, name);
+
+    pubsub.publish(AUTHOR_UPDATED, {
+      authorUpdated: updatedAuthor,
+    });
+
     return updatedAuthor;
   },
+
   deleteBook: async (_, { bookId }) => {
     const deletedBook = await deleteBook(bookId);
+
+    pubsub.publish(BOOK_DELETED, {
+      bookDeleted: deletedBook,
+    });
+
     return deletedBook;
   },
+
   deleteAuthor: async (_, { authorId }) => {
     const deletedAuthor = await deleteAuthor(authorId);
+
+    pubsub.publish(AUTHOR_DELETED, {
+      authorDeleted: deletedAuthor,
+    });
+
     return deletedAuthor;
   },
 };
