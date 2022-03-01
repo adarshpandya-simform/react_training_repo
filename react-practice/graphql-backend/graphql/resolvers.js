4980c7a31f53dad5e0598db5f41532b418c74d85
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-03-01 11:42:03 +0530

    TE5-T197 added: Subscription and custom query types in resolver

--- react-practice/graphql-backend/graphql/resolvers.js
+++ react-practice/graphql-backend/graphql/resolvers.js
@@ -1,26 +1,14 @@
-import { getAuthor, getAuthors } from "../helpers/author.helper.js";
-import { getBook, getBooks } from "../helpers/book.helper.js";
+import { bookQueryResolver } from "./customTypes/book.query.js";
+import { authorQueryResolver } from "./customTypes/author.query.js";
 import { mutations } from "./mutations.js";
+import { queries } from "./queries.js";
+import { subscriptions } from "./subscriptions.js";
 
 // resolvers for gql
 export const resolvers = {
-  Query: {
-    books: async () => await getBooks(),
-    book: async (_, args) => await getBook(args.id),
-    authors: async () => await getAuthors(),
-    author: async (_, args) => await getAuthor(args.id),
-  },
+  Query: queries,
   Mutation: mutations,
-  Book: {
-    async author(parent) {
-      const fetchedAuthor = await getAuthor(parent.id);
-      return fetchedAuthor;
-    },
-  },
-  Author: {
-    async books(parent) {
-      const fetchedBook = await getBook(parent.authorId);
-      return fetchedBook;
-    },
-  },
+  Book: bookQueryResolver,
+  Author: authorQueryResolver,
+  Subscription: subscriptions,
 };
