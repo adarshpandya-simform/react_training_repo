2c40c59a861050a022a2d1d3dcef5edf22c74708
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-03-01 11:36:11 +0530

    TE5-T197 added: triggers for subscriptions

---
+++ react-practice/graphql-backend/graphql/triggers.js
@@ -0,0 +1,9 @@
+// subscription triggers for book
+export const BOOK_CREATED = "BOOK_CREATED";
+export const BOOK_DELETED = "BOOK_DELETED";
+export const BOOK_UPDATED = "BOOK_UPDATED";
+
+// subscription triggers for author
+export const AUTHOR_CREATED = "AUTHOR_CREATED";
+export const AUTHOR_DELETED = "AUTHOR_DELETED";
+export const AUTHOR_UPDATED = "AUTHOR_UPDATED";
