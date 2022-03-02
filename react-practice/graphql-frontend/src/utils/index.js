616efd6f1a84683bf81c75695de1305600c0c303
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-03-02 17:07:06 +0530

    TE5-T197 exported: all util helpers from index.js

---
+++ react-practice/graphql-frontend/src/utils/index.js
@@ -0,0 +1,5 @@
+// exporting all the util functions
+export { BASE_URL } from "./getBaseUrl";
+export { client } from "./getApolloClient";
+export { trimText } from "./getStringTrimmer";
+export { getBooksString } from "./getBooksString";
