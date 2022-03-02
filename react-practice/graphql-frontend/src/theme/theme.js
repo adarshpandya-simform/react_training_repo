9dacea2043983196438a4856a6418c1b8baa8489
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-03-02 17:09:12 +0530

    TE5-T197 created: custom material ui theme

---
+++ react-practice/graphql-frontend/src/theme/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+// creating and exporting custom material ui theme
+export const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#E00082",
+    },
+    secondary: {
+      main: "#172A3A",
+    },
+  },
+});
