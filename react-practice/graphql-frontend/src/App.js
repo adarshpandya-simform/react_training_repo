cc32f42bf62e888770c2f12527b035bf8a4340a3
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-03-02 17:20:00 +0530

    TE5-T197 created: App component with routing, rendered: all components on their respective routes

--- react-practice/graphql-frontend/src/App.js
+++ react-practice/graphql-frontend/src/App.js
@@ -1,7 +1,31 @@
+import { Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
+import {
+  AddAuthor,
+  AddBook,
+  AuthorList,
+  BookList,
+  DeleteAuthor,
+  DeleteBook,
+  UpdateAuthor,
+  UpdateBook,
+} from "./components";
 
+// App component with all the routes
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/books" />} />
+      <Route path="/books" element={<BookList />} />
+      <Route path="/authors" element={<AuthorList />} />
+      <Route path="/add-book" element={<AddBook />} />
+      <Route path="/add-author" element={<AddAuthor />} />
+      <Route path="/delete-book" element={<DeleteBook />} />
+      <Route path="/delete-author" element={<DeleteAuthor />} />
+      <Route path="/update-book" element={<UpdateBook />} />
+      <Route path="/update-author" element={<UpdateAuthor />} />
+    </Routes>
+  );
 };
 
 export default App;
