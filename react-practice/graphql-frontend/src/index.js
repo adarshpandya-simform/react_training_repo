7112a00944e7055d96a7fbfe2dfdba2a7ced450f
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-03-02 17:20:58 +0530

    TE5-T197 wrapped: App component with router, layout and providers

--- react-practice/graphql-frontend/src/index.js
+++ react-practice/graphql-frontend/src/index.js
@@ -1,11 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "./utils";
+import { Layout } from "./components";
+import { theme } from "./theme/theme";
+import { ThemeProvider } from "@mui/material";
+import { SnackbarProvider } from "notistack";
 import "./index.css";
 
+// Wrapping up App with Router and Providers and Layout
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <ApolloProvider client={client}>
+        <SnackbarProvider>
+          <ThemeProvider theme={theme}>
+            <Layout>
+              <App />
+            </Layout>
+          </ThemeProvider>
+        </SnackbarProvider>
+      </ApolloProvider>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
