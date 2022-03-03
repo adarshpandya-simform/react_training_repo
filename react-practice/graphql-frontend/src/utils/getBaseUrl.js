cf4b6c3a2feae143ddb2296955958e93d412f0e7
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-03-03 13:34:02 +0530

    TE5-T197 added: base url for web sockets

--- react-practice/graphql-frontend/src/utils/getBaseUrl.js
+++ react-practice/graphql-frontend/src/utils/getBaseUrl.js
@@ -1,2 +1,5 @@
 // get base graphql url
 export const BASE_URL = "http://localhost:2000/graphql";
+
+// get base graphql url for websocket subscriptions
+export const WEB_SOCKET_BASE_URL = "ws://localhost:2000/graphql";
