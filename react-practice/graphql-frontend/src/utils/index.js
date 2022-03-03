8c4025e3a5388cfae5fcd1fa4eb6aa1de8f0de38
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-03-03 13:35:27 +0530

    TE5-T197 updated: index for utils

--- react-practice/graphql-frontend/src/utils/index.js
+++ react-practice/graphql-frontend/src/utils/index.js
@@ -1,5 +1,13 @@
 // exporting all the util functions
-export { BASE_URL } from "./getBaseUrl";
+export { BASE_URL, WEB_SOCKET_BASE_URL } from "./getBaseUrl";
 export { client } from "./getApolloClient";
 export { trimText } from "./getStringTrimmer";
 export { getBooksString } from "./getBooksString";
+export {
+  authorCreatedMsg,
+  authorDeletedMsg,
+  authorUpdatedMsg,
+  bookCreatedMsg,
+  bookDeletedMsg,
+  bookUpdatedMsg,
+} from "./getSnackbarMsgs";
