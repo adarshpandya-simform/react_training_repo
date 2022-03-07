d647127b887bd1407c8404f794cca44d9292c6d1
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-03-07 15:45:48 +0530

    exported: all hooks from index.js

---
+++ react-practice/todo-frontend-rq/src/hooks/index.js
@@ -0,0 +1,6 @@
+// exporting all the hooks
+export { useTodo } from "./useTodo";
+export { useTodos } from "./useTodos";
+export { useCreateTodo } from "./useCreateTodo";
+export { useDeleteTodo } from "./useDeleteTodo";
+export { useUpdateTodo } from "./useUpdateTodo";
