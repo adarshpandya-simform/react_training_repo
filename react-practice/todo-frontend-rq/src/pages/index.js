b6bccd18d45bb6ce1695051155f8461c41a969a1
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-03-07 15:52:41 +0530

    TE5-T197 exported: all pages from pages folder

---
+++ react-practice/todo-frontend-rq/src/pages/index.js
@@ -0,0 +1,5 @@
+// exporting all pages
+export { default as HomePage } from "./HomePage";
+export { default as TodosPage } from "./TodosPage";
+export { default as TodoDetailPage } from "./TodoDetailPage";
+export { default as AddTodoPage } from "./AddTodoPage.js";
