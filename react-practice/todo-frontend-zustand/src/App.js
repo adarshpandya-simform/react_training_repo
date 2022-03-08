45f9ba217fe44ae688f54f6ef93bd0dda3b692d7
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-03-08 17:27:43 +0530

    TE5-T197 added: simple Header component for navigation

--- react-practice/todo-frontend-zustand/src/App.js
+++ react-practice/todo-frontend-zustand/src/App.js
@@ -1,7 +1,32 @@
-import React from "react";
+import { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import { useStore } from "./store/useStore";
+import Header from "./components/Header/Header";
+import TodosPage from "./pages/TodosPage";
+import TodoDetailPage from "./pages/TodoDetailPage";
+import AddTodoPage from "./pages/AddTodoPage";
 
 const App = () => {
-  return <div>App</div>;
+  const getTodos = useStore((state) => state.getTodos);
+
+  useEffect(() => {
+    (async () => {
+      await getTodos();
+    })();
+  }, [getTodos]);
+
+  return (
+    <div className="app-container">
+      <Header />
+      <Routes>
+        <Route path="/" element={<p>this is home page</p>} />
+        <Route path="/todos" element={<TodosPage />} />
+        <Route path="/todo/:id" element={<TodoDetailPage />} />
+        <Route path="/add-todo" element={<AddTodoPage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
