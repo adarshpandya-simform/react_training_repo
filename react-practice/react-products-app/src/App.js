4eb2f1e5c124b8cad1e5d48db767a3ebd327404c
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-03-09 18:06:59 +0530

    TE5-T197 rendered: pages on their routes

--- react-practice/react-products-app/src/App.js
+++ react-practice/react-products-app/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import { Layout } from "./components";
+import {
+  AddProductPage,
+  HomePage,
+  ProductDetailsPage,
+  ProductEditPage,
+  ProductsPage,
+} from "./pages";
+import { useProduct } from "./hooks/useProduct";
 
-function App() {
+const App = () => {
+  const { fetchProducts } = useProduct();
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/add-product" element={<AddProductPage />} />
+        <Route path="/product/:id" element={<ProductDetailsPage />} />
+        <Route path="/edit-product/:id" element={<ProductEditPage />} />
+      </Routes>
+    </Layout>
   );
-}
+};
 
 export default App;
