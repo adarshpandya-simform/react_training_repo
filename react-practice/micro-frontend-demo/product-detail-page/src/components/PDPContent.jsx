3ec2038c2f60e866baf8a28c5233bcf42ae8d611
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-03-11 17:36:23 +0530

    TE5-T197 created: PDPContent component which consumes shared functions

---
+++ react-practice/micro-frontend-demo/product-detail-page/src/components/PDPContent.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import { getProduct, currency } from "home/products";
+import { useParams } from "react-router-dom";
+
+const PDPContent = () => {
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    if (id) {
+      getProduct(id).then((product) => {
+        setProduct(product);
+      });
+    } else {
+      setProduct(null);
+    }
+  }, []);
+
+  if (!product) return null;
+
+  return (
+    <div className="grid grid-cols-2 gap-5">
+      <div>
+        <img src={product.image} alt={product.name} />
+      </div>
+      <div>
+        <div className="flex">
+          <h1 className="font-bold text-3xl flex-grow">{product.name}</h1>
+          <div className="font-bold text-3xl flex-end">
+            {currency.format(product.price)}
+          </div>
+        </div>
+        <div className="mt-10">{product.description}</div>
+        <div className="mt-10">{product.longDescription}</div>
+      </div>
+    </div>
+  );
+};
+
+export default PDPContent;
