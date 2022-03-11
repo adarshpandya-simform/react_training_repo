2ce8b06f39b1e00ff082a4e2098a9eaf37017f9c
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-03-11 17:36:59 +0530

    TE5-T197 rendered: PDPContent with Proper routing

--- react-practice/micro-frontend-demo/product-detail-page/src/App.jsx
+++ react-practice/micro-frontend-demo/product-detail-page/src/App.jsx
@@ -1,33 +1,27 @@
 import React, { lazy, Suspense, useState } from "react";
 import ReactDOM from "react-dom";
 import Footer from "home/Footer";
-// import Header from "home/Header";
-const Header = lazy(() => import("home/Header"));
+import Header from "home/Header";
+import ErrorBoundary from "./components/ErrorBoundary";
+import PDPContent from "./components/PDPContent";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./index.scss";
 
 const App = () => {
-  const [showHeader, setShowHeader] = useState(false);
   return (
-    <div className="text-3xl mx-auto max-w-6xl">
-      {showHeader && (
-        <Suspense fallback={<p>loading...</p>}>
+    <Router>
+      <div className="text-3xl mx-auto max-w-6xl">
+        <ErrorBoundary>
           <Header />
-        </Suspense>
-      )}
-
-      <div className="my-10">
-        <button
-          className="bg-red-700 text-xl text-black"
-          onClick={() => {
-            setShowHeader((prevState) => !prevState);
-          }}
-        >
-          toggle header
-        </button>
-        Product Page Content
+        </ErrorBoundary>
+        <div className="my-10">
+          <Routes>
+            <Route path="/product/:id" element={<PDPContent />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Router>
   );
 };
 ReactDOM.render(<App />, document.getElementById("app"));
