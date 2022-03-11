54de91bd537cc6288ea32a6366d05167cd236175
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-03-11 17:34:12 +0530

    TE5-T197 rendered: Header and footer in App

--- react-practice/micro-frontend-demo/home/src/App.jsx
+++ react-practice/micro-frontend-demo/home/src/App.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HomeContent from "./components/HomeContent";
 
 import "./index.scss";
 
 const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: home</div>
-    <div>Framework: react</div>
-    <div>Language: JavaScript</div>
-    <div>CSS: Tailwind</div>
+  <div className="text-3xl mx-auto max-w-6xl">
+    <Header />
+    <div className="my-10">
+      <HomeContent />
+    </div>
+    <Footer />
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
