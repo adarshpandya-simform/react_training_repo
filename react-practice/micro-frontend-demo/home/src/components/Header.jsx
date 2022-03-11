7e20f3af208b56e5d794af6c146c6fb7687602f8
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-03-11 17:34:36 +0530

    TE5-T197 created: simple components for testing

---
+++ react-practice/micro-frontend-demo/home/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="p-5 bg-blue-400 text-white text-2xl font-bold">
+      Fidget Spinner World!!!
+    </div>
+  );
+};
+
+export default Header;
---
+++ react-practice/micro-frontend-demo/home/src/components/HomeContent.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import { getProducts, currency } from "../api/products.api";
+
+const HomeContent = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts().then(setProducts);
+  }, []);
+
+  return (
+    <div className="grid grid-cols-4 gap-5">
+      {products.map((product) => (
+        <div key={product.id}>
+          <img src={product.image} alt={product.name} />
+          <div className="flex">
+            <div className="flex-grow font-bold">
+              <a>{product.name}</a>
+            </div>
+            <div className="flex-end">{currency.format(product.price)}</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default HomeContent;
