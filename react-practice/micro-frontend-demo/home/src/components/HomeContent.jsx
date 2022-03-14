ca3a2c705c4e25d2f89a633347d18e813bd3bbae
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-03-14 17:47:18 +0530

    TE5-T197 refactored: home mfe components

--- react-practice/micro-frontend-demo/home/src/components/HomeContent.jsx
+++ react-practice/micro-frontend-demo/home/src/components/HomeContent.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { getProducts, currency } from "../api/products.api";
+import { addToCart, useLoggedIn } from "cart/cart";
 
 const HomeContent = () => {
+  const loggedIn = useLoggedIn();
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
@@ -19,6 +21,18 @@ const HomeContent = () => {
             </div>
             <div className="flex-end">{currency.format(product.price)}</div>
           </div>
+          <div className="text-sm mt-4">{product.description}</div>
+          {loggedIn && (
+            <div className="text-right mt-2">
+              <button
+                className="bg-blue-400 hover:bg-blue-700 text-white text-sm font-bold py-2 px-4 rounded"
+                onClick={() => addToCart(product.id)}
+                id={`addtocart_${product.id}`}
+              >
+                Add to Cart
+              </button>
+            </div>
+          )}
         </div>
       ))}
     </div>
