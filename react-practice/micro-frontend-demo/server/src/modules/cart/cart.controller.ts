b80cd2ed479ae72f05bd696bb4ec0ef8cef015a4
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-03-14 13:35:42 +0530

    TE5-T197 added: POST and DELETE method in nest

--- react-practice/micro-frontend-demo/server/src/modules/cart/cart.controller.ts
+++ react-practice/micro-frontend-demo/server/src/modules/cart/cart.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { JwtAuthGuard } from '../../auth/jwt-auth.guard';
 
 import products, { Product } from 'src/products';
@@ -30,5 +38,28 @@ export class CartController {
   @UseGuards(JwtAuthGuard)
   async index(@Request() req): Promise<Cart> {
     return this.carts[req.user.userId] ?? { cartItems: [] };
+  }
+
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  async create(@Request() req, @Body() { id }: { id: string }) {
+    const cart = this.carts[req.user.userId];
+    const cartItem = cart.cartItems.find((cartItem) => cartItem.id === +id);
+    if (cartItem) {
+      cartItem.quantity += 1;
+    } else {
+      cart.cartItems.push({
+        ...products.find((product) => product.id === +id),
+        quantity: 1,
+      });
+    }
+    return cart;
+  }
+
+  @Delete()
+  @UseGuards(JwtAuthGuard)
+  async destroy(@Request() req): Promise<Cart> {
+    this.carts[req.user.userId] = { cartItems: [] };
+    return this.carts[req.user.userId];
   }
 }
