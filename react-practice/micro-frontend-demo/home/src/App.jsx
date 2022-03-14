8461b10882265d003f0bc2c2353f9ccd84b610f8
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-03-14 17:47:47 +0530

    TE5-T197 added: remixicon fonts

--- react-practice/micro-frontend-demo/home/src/App.jsx
+++ react-practice/micro-frontend-demo/home/src/App.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomeContent from "./components/HomeContent";
-
+import "remixicon/fonts/remixicon.css";
 import "./index.scss";
 
 const App = () => (
