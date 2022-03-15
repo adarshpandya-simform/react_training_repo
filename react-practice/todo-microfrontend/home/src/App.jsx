0022d9a46416133a6c34e35ee5aa42e058b179f0
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-03-15 18:36:05 +0530

    TE5-T197 rendered: All components from differents mfs

--- react-practice/todo-microfrontend/home/src/App.jsx
+++ react-practice/todo-microfrontend/home/src/App.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import Header from "layout/Header";
+import Footer from "layout/Footer";
+import ReduxWrapper from "add_todo/ReduxWrapper";
+import AddTodo from "add_todo/AddTodo";
+import CompletedTodos from "completed_todos/CompletedTodos";
+import IncompleteTodos from "incompleted_todos/IncompleteTodos";
 import "./index.css";
 
 const App = () => (
-  <div className="container">
-    <div>Name: home</div>
-    <div>Framework: react</div>
-    <div>Language: JavaScript</div>
-    <div>CSS: Empty CSS</div>
+  <div>
+    <Header />
+    <br />
+    <AddTodo />
+    <br />
+    <CompletedTodos />
+    <br />
+    <IncompleteTodos />
+    <br />
+    <Footer />
   </div>
 );
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(
+  <ReduxWrapper>
+    <App />
+  </ReduxWrapper>,
+  document.getElementById("app")
+);
