1f3a93f2a259f9d7d2fd79ce58a3070d7f358b1b
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-03-17 17:01:41 +0530

    TE5-T197 added: remotes for all the microfrontends

---
+++ react-practice/fullstack-microfrontend/microfrontends/app/webpack.config.js
@@ -0,0 +1,69 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+const deps = require("./package.json").dependencies;
+module.exports = {
+  output: {
+    publicPath: "http://localhost:3000/",
+  },
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+  },
+
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        type: "javascript/auto",
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "app",
+      filename: "remoteEntry.js",
+      remotes: {
+        layout: "layout@http://localhost:3002/remoteEntry.js",
+        data_store: "data_store@http://localhost:3001/remoteEntry.js",
+        add_user: "add_user@http://localhost:3004/remoteEntry.js",
+        users: "users@http://localhost:3003/remoteEntry.js",
+      },
+      exposes: {},
+      shared: {
+        ...deps,
+        react: {
+          singleton: true,
+          requiredVersion: deps.react,
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: deps["react-dom"],
+        },
+      },
+    }),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
