2f51215930f96fed9f1e8a4f03c9b9f07ac053cf
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-03-17 16:50:18 +0530

    TE5-T197 added: layout components for app mfe

---
+++ react-practice/fullstack-microfrontend/microfrontends/layout/src/components/ChakraWrapper.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "../theme/theme";
+
+const ChakraWrapper = ({ children }) => {
+  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
+};
+
+export default ChakraWrapper;
