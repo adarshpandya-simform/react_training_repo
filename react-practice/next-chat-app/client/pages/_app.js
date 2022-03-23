4a2d4877fc9a065ad250965a97c623f4570f3ef5
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-03-23 10:16:51 +0530

    wrapped: component with Layout and UserContextProvider

--- react-practice/next-chat-app/client/pages/_app.js
+++ react-practice/next-chat-app/client/pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import { UserContextProvider } from "../context/UserContext";
+import Layout from "../layout/Layout";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <UserContextProvider>
+        <Component {...pageProps} />
+      </UserContextProvider>
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
