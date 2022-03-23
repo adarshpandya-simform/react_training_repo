eb66ce1b8297d64723106985bb9dfc870821dc32
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-03-23 16:40:50 +0530

    TE5-T197 rendered: all pages on respective routes

--- react-practice/formik-practice/src/App.tsx
+++ react-practice/formik-practice/src/App.tsx
@@ -1,7 +1,27 @@
+import { AnimateSharedLayout } from "framer-motion";
+import { FC } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import { AuthPageRenderer } from "./pages";
 import "./App.css";
 
-const App = () => {
-  return <div>App</div>;
+const App: FC = () => {
+  return (
+    <AnimateSharedLayout>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Link to="/login">login</Link>
+              <Link to="/signup">signup</Link>
+            </>
+          }
+        />
+        <Route path="/login" element={<AuthPageRenderer type="login" />} />
+        <Route path="/signup" element={<AuthPageRenderer type="signup" />} />
+      </Routes>
+    </AnimateSharedLayout>
+  );
 };
 
 export default App;
