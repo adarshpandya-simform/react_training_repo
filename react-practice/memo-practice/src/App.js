d76c4c1c5fc292864ac6a5bdb25902f23d88678b
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-03-24 18:09:59 +0530

    TE5-T197 rendered: all components

--- react-practice/memo-practice/src/App.js
+++ react-practice/memo-practice/src/App.js
@@ -1,7 +1,48 @@
+import { useState } from "react";
 import "./App.css";
+import { MemoizedComponent, NormalComponent } from "./components";
+import MemoizedComponentTwo from "./components/MemoizedComponentTwo";
+import NormalComponentTwo from "./components/NormalComponentTwo";
 
 const App = () => {
-  return <div>App</div>;
+  const [renderedCount, setRenderedCount] = useState(0);
+  const [color, setColor] = useState("red");
+  const [title, setTitle] = useState("");
+  console.log(`app rendered: ${renderedCount}`);
+
+  return (
+    <div>
+      <div>
+        <button
+          onClick={() => {
+            setRenderedCount((renderedCount) => renderedCount + 1);
+          }}
+        >
+          re render app
+        </button>
+        <button
+          onClick={() => {
+            setColor(color === "red" ? "blue" : "red");
+          }}
+        >
+          change color
+        </button>
+        <br />
+        <input
+          type={"text"}
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Enter Title"
+        />
+      </div>
+      <div>
+        <NormalComponent color={color} />
+        <MemoizedComponent color={color} />
+        <NormalComponentTwo />
+        <MemoizedComponentTwo />
+      </div>
+    </div>
+  );
 };
 
 export default App;
